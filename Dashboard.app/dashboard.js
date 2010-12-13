1f131400d70cbe1c07191d897de1141c16ea1213
Jeremie Miller <jeremie@jabber.org>
2010-12-12 20:51:02 -0600

    first thoughts of a dashboard app

---
+++ Dashboard.app/dashboard.js
@@ -0,0 +1,58 @@
+var fs = require('fs'),
+    path = require('path'),
+    url = require('url'),
+    sys = require('sys'),
+    express = require('express'),
+    connect = require('connect');
+    
+var app = express.createServer(
+                connect.bodyDecoder(),
+                connect.cookieDecoder(),
+                connect.session()
+            );
+
+
+var resCount=0;
+function readJSON(res,file) {
+    var data = fs.readFileSync(file, "utf-8");
+    var lines = data.split('\n');
+    res.write("found "+lines.length+" entries in "+file);
+    resCount--;
+    
+}
+
+app.get('/', function (req, res) {    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><head><title>Locker Dashboard</title></head>\n\n<body>');
+    res.end("</body></html>");
+});
+
+app.get('/*', function (req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), uri);  
+    path.exists(filename, function(exists) { 
+        if(!exists) {  
+            res.writeHead(404, {"Content-Type": "text/plain"});  
+            res.write("404 Not Found\n");  
+            res.end();  
+            return;  
+        }  
+
+        fs.readFile(filename, "binary", function(err, file) {  
+            if(err) {  
+                res.writeHead(500, {"Content-Type": "text/plain"});  
+                res.write(err + "\n");  
+                res.end();  
+                return;  
+            }  
+
+            res.writeHead(200);  
+            res.write(file, "binary");  
+            res.end();  
+        });  
+    });
+});
+
+console.log('Server running at http://127.0.0.1:3002/');
+
+app.listen(3002);
--- YouTube.journal/vids.js
+++ YouTube.journal/vids.js
@@ -24,20 +24,22 @@ function get(host, url, token, callback) {
     });
 }
 
-// curl "http://gdata.youtube.com/feeds/api/users/default/uploads?alt=json&v=2" --header 'Authorization: GoogleLogin auth=PzgWvEBx...I83qvkHJjYYha_A' > qjyoutube2.js
+function save(file, data) {
+    var js = JSON.parse(data);
+    fs.mkdir('my',0755);
+    stream = fs.createWriteStream('my/'+file+'.json');
+    console.log("saving "+file+" metadata about "+js.feed.entry.length+" vids");
+    for(var i=0;i<js.feed.entry.length;i++)
+    {
+        stream.write(JSON.stringify(js.feed.entry[i]) + "\n");
+    }
+    stream.end();
+};
+
 fs.readFile("auth.token", "utf-8",
 function(err, token) {
     console.log("loaded token " + token);
-    get('gdata.youtube.com', '/feeds/api/users/default/uploads?alt=json&v=2', token,
-    function(data) {
-        var js = JSON.parse(data);
-        fs.mkdir('my',0755);
-        stream = fs.createWriteStream('my/videos.json');
-        console.log("saving metadata about "+js.feed.entry.length+" vids");
-        for(var i=0;i<js.feed.entry.length;i++)
-        {
-            stream.write(JSON.stringify(js.feed.entry[i]) + "\n");
-        }
-        stream.end();
-    });
+    get('gdata.youtube.com', '/feeds/api/users/default/uploads?alt=json&v=2', token,function(data){save('videos',data);});
+    get('gdata.youtube.com', '/feeds/api/users/default/subscriptions?alt=json&v=2', token,function(data){save('subscriptions',data);});
+    get('gdata.youtube.com', '/feeds/api/users/default/newsubscriptionvideos?alt=json&v=2', token,function(data){save('newsubscriptionvideos',data);});
 });
