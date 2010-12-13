1d20cc263af26554eee721248c6662a00cb66a73
Jeremie Miller <jeremie@jabber.org>
2010-12-12 19:08:31 -0600

    youtube actually saves vid metadata out now

--- YouTube.journal/vids.js
+++ YouTube.journal/vids.js
@@ -3,10 +3,11 @@ var http = require('http');
 
 
 function get(host, url, token, callback) {
-    var httpClient = http.createClient(80, host, true);
+    var httpClient = http.createClient(80, host);
     var request = httpClient.request('GET', url, {
         host: host,
-        'Authorization':'GoogleLogin auth='+token
+        'Connection': 'close',
+        'Authorization': 'GoogleLogin auth=' + token
     });
     request.end();
     request.on('response',
@@ -24,10 +25,19 @@ function get(host, url, token, callback) {
 }
 
 // curl "http://gdata.youtube.com/feeds/api/users/default/uploads?alt=json&v=2" --header 'Authorization: GoogleLogin auth=PzgWvEBx...I83qvkHJjYYha_A' > qjyoutube2.js
-
-fs.readFile("auth.token", "utf-8", function(err, token) {
+fs.readFile("auth.token", "utf-8",
+function(err, token) {
     console.log("loaded token " + token);
-    get('gdata.youtube.com','/feeds/api/users/default/uploads?alt=json&v=2',token,function(data){
-        console.log(data);
+    get('gdata.youtube.com', '/feeds/api/users/default/uploads?alt=json&v=2', token,
+    function(data) {
+        var js = JSON.parse(data);
+        fs.mkdir('my',0755);
+        stream = fs.createWriteStream('my/videos.json');
+        console.log("saving metadata about "+js.feed.entry.length+" vids");
+        for(var i=0;i<js.feed.entry.length;i++)
+        {
+            stream.write(JSON.stringify(js.feed.entry[i]) + "\n");
+        }
+        stream.end();
     });
 });
