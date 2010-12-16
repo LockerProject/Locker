916b8b27e5be3abf1f8ed7b870f0db9018e17de2
Simon Murtha-Smith <Simon@Simons-MacBook-Pro.local>
2010-12-15 19:02:29 -0500

    Preliminary Chrome history. :)

---
+++ chromeHistory.journal/client.js
@@ -0,0 +1,26 @@
+/**
+ * Module dependencies.
+ */
+
+var fs = require('fs');
+var express = require('express'),
+connect = require('connect'),
+app = express.createServer(
+    connect.bodyDecoder(),
+    connect.cookieDecoder(),
+    connect.session());
+
+app.post('/urls',
+    function(req, res) {
+        fs.mkdir('my', 0755);
+        var stream = fs.createWriteStream('my/history.json');
+        var json = JSON.parse(req.body.urls);
+        for(var i = 0; i <  json.length; i++) {
+            stream.write(JSON.stringify(json[i]) + '\n');
+        }
+        stream.end();
+    });
+
+
+console.log("server running at http://localhost:3005/");
+app.listen(3005);
