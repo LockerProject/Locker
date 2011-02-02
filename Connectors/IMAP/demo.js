41d7453daf4e4065eea1cd620e5480e9b0dd4977
Jeremie Miller <jeremie@jabber.org>
2011-02-01 18:17:58 -0600

    simple imap demo data server

---
+++ Connectors/IMAP/demo.js
@@ -0,0 +1,35 @@
+/**
+ * Module dependencies.
+ */
+
+var port = 3005;
+
+var fs = require('fs'),http = require('http');
+var express = require('express'),connect = require('connect');
+var app = express.createServer(connect.bodyDecoder(), connect.cookieDecoder(), connect.session());
+var lfs = require('../../Common/node/lfs.js');
+
+
+app.set('views', __dirname);
+
+app.get('/',
+function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end("yeah, hello world, and stuff");
+});
+
+app.get('/get_home_timeline', function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/javascript'
+    });
+    lfs.readObjectsFromFile('demo.json', function(data) {
+        data.reverse();
+        res.write(JSON.stringify(data));
+        res.end();
+    });
+});
+
+console.log("http://localhost:"+port+"/");
+app.listen(port);
