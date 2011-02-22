fdbc174be7f5f754f14efe8a08788790c1418090
Simon Murtha-Smith <simon@murtha-smith.com>
2011-02-22 17:06:29 -0500

    bits and bobs

--- Connectors/ChromeHistory/client.js
+++ Connectors/ChromeHistory/client.js
@@ -1,26 +1,43 @@
 /**
  * Module dependencies.
  */
+var cwd = process.argv[2];
+var port = process.argv[3];
+if (!cwd || !port) {
+    process.stderr.write("missing dir and port arguments\n");
+    process.exit(1);
+}
+process.chdir(cwd);
 
 var fs = require('fs');
 var express = require('express'),
-connect = require('connect'),
-app = express.createServer(
-    connect.bodyDecoder(),
-    connect.cookieDecoder(),
-    connect.session({secret : "locker"}));
+    connect = require('connect'),
+    lfs = require('../../Common/node/lfs.js'),
+    app = express.createServer(
+        connect.bodyDecoder(),
+        connect.cookieDecoder(),
+        connect.session({secret : "locker"}));
+
+var me = lfs.loadMeData();
 
 app.post('/urls',
-    function(req, res) {
-        fs.mkdir('my', 0755);
-        var stream = fs.createWriteStream('my/history.json');
-        var json = JSON.parse(req.body.urls);
-        for(var i = 0; i <  json.length; i++) {
-            stream.write(JSON.stringify(json[i]) + '\n');
-        }
-        stream.end();
-    });
+function(req, res) {
+    var stream = fs.createWriteStream('history.json');
+    var json = JSON.parse(req.body.urls);
+    for(var i = 0; i <  json.length; i++) {
+        stream.write(JSON.stringify(json[i]) + '\n');
+    }
+    me.latest = json[0];
+    me.oldest = json[json.length - 1];
+    lfs.
+    stream.end();
+});
+
+app.get('/latest', 
+function(req, res) {
+    
+});
 
 
-console.log("server running at http://localhost:3005/");
-app.listen(3005);
+console.log("server running at http://localhost:" + port + "/");
+app.listen(port);
