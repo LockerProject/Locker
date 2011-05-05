408150a08b25ffdaa4706cfb07f2524b31b0462b
Thomas Muldowney <temas@sparecog.com>
2011-05-05 17:37:47 -0500

    Not actually in PyPI yet.  Requires hand installation still.

--- setupEnv.py
+++ setupEnv.py
@@ -3,4 +3,3 @@ from setuptools.command import easy_install
 easy_install.main(["-U", "gdata"])
 easy_install.main(["-U", "flask"])
 easy_install.main(["-U", "pyparsing"])
-easy_install.main(["-U", "sleekxmpp"])
