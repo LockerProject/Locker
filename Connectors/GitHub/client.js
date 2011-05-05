6deada08f5d06ec4c55ee26749fb9d7e566b3cea
Simon Murtha-Smith <simon@murtha-smith.com>
2011-05-05 15:40:42 -0700

    VERY basic Github connector (so, meta).

---
+++ Connectors/GitHub/client.js
@@ -0,0 +1,101 @@
+/*
+*
+* Copyright (C) 2011, The Locker Project
+* All rights reserved.
+*
+* Please see the LICENSE file for more information.
+*
+*/
+
+/**
+ * web server/service to wrap interactions w/ GitHub API
+ */
+
+var fs = require('fs'),
+    url = require('url'),
+    express = require('express'),
+    connect = require('connect'),
+    sys = require('sys'),
+    app = express.createServer(
+                    connect.bodyParser(),
+                    connect.cookieParser(),
+                    connect.session({secret : "locker"})),
+    locker = require('../../Common/node/locker.js'),
+    lfs = require('../../Common/node/lfs.js');
+    
+var ghlib = require('./lib.js');
+
+var me, auth, github;
+
+app.set('views', __dirname);
+app.get('/',
+function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    if(!auth.username) {
+        res.end("<html>Enter your personal GitHub username" +
+                "<form method='post' action='/save'>" +
+                    "Username: <input name='username'><br>" +
+                    "<input type='submit' value='Save'>" +
+                "</form></html>");
+        return;
+    } else
+        res.end("<html>We've got a username, <a href='./profile'>load p</a></html>");
+});
+
+
+app.post('/save',
+function(req, res) {
+    if(!req.param('username')) {
+        res.writeHead(400);
+        res.end("missing field(s)?");
+        return;
+    }
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+    auth.username = req.param('username');
+    lfs.writeObjectToFile('auth.json', auth);
+    res.end("<html>k thanks. <a href='/'>head home</a></html>");
+});
+
+app.get('/syncrepo/:repo', function(req, res) {
+    var repo = req.params.repo;
+    if(!repo) {
+        res.writeHead(400);
+        res.end('need repo info dood!');
+        return;
+    }
+    getGitHub().syncWatchersInfo(repo);
+    res.writeHead(200);
+    res.end();
+});
+
+
+function getGitHub() {
+    if(!github) {
+        github = ghlib.createClient('quartzjer');
+        github.on('new-watcher', function(newWatcherEvent) {
+            console.log('got new watcher:', newWatcherEvent);
+            locker.event('contact/github', newWatcherEvent);
+        });
+    }
+    return github;
+}
+
+var stdin = process.openStdin();
+stdin.setEncoding('utf8');
+stdin.on('data', function (chunk) {
+    var processInfo = JSON.parse(chunk);
+    locker.initClient(processInfo);
+    process.chdir(processInfo.workingDirectory);
+    me = lfs.loadMeData();
+    lfs.readObjectFromFile('auth.json', function(newAuth) {
+        auth = newAuth;
+        app.listen(processInfo.port,function() {
+            var returnedInfo = {port: processInfo.port};
+            console.log(JSON.stringify(returnedInfo));
+        });
+    });
+});
---
+++ Connectors/GitHub/lib.js
@@ -0,0 +1,49 @@
+var github = require('./github-lib.js');
+var fs = require('fs');
+var lfs = require('../../Common/node/lfs.js');
+var EventEmitter = require('events').EventEmitter;
+var locker = require('../../Common/node/locker.js');
+
+exports.createClient = function(username) {
+    return new GHClient(username);
+}
+
+
+
+function GHClient(username) {
+    this.username = username;
+    return this;
+}
+
+GHClient.prototype = new EventEmitter();
+
+GHClient.prototype.syncWatchersInfo = function(repo, callback) {
+    console.log(this.username);
+    var self = this;
+    lfs.readObjectFromFile('repo-' + repo + '-watchersIDs.json', function(watchersIDs) {
+        console.log(watchersIDs);
+        github.getNewWatchers(self.username, repo, watchersIDs, function(newWatchers) {
+//            if(!err) {
+                //sync watchers to disk
+                lfs.appendObjectsToFile(self.username + '-' + repo + '-watchers.json', newWatchers);
+                //determine which watchers are new, emit events, and sync watchersIDs to disk
+                lfs.readObjectFromFile(self.username + '-' + repo + '-watchersIDs.json', function(watchersIDs) {
+                    newWatchers.forEach(function(watcher) {
+                        if(!watchersIDs[watcher.login]) {
+                            watchersIDs[watcher.login] = true;
+                            self.emit('new-watcher', {repo:{username:self.username, reponame:repo}, user:watcher});
+                            //emit event
+                        }
+                    });
+                    lfs.writeObjectToFile('repo-' + repo + '-watchersIDs.json', watchersIDs);
+                    if(callback) callback();
+                });
+            // }
+        });
+    });
+}
+
+var userInfoQueue = [];
+function enqueueUserInfoRequest(username) {
+    userInfoQueue.push(username)
+}
