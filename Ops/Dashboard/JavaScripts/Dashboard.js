e5eef7e22e8074e172f5c372215d60aadd4dee33
Justin Mecham <justin@mecham.net>
2011-05-09 19:55:33 -0400

    Separated the CSS for each of the sections and other cleanups.

--- Ops/Dashboard/JavaScripts/Dashboard.js
+++ Ops/Dashboard/JavaScripts/Dashboard.js
@@ -9,58 +9,57 @@
 
 var serviceMap;
 
-function selectAvailable(index) {
-    var item = serviceMap.available[index];
-    $("#availInfo h1").html(item["title"]);
-    $("#availInfo p").html(item["desc"]);
-    $("#availSrcDir").html(item["srcdir"]);
-    if (item["provides"]) $("#availProvides").html(item["provides"].join(","));
-    $("#availInstall").attr("href", "javascript:install(" + index + ");");
+function selectAvailable(index)
+{
+  var item = serviceMap.available[index];
+  $("#connectorInfo h1").html(item["title"]);
+  $("#connectorInfo p").html(item["desc"]);
+  $("#availSrcDir").html(item["srcdir"]);
+  if (item["provides"]) $("#availProvides").html(item["provides"].join(","));
+  $("#availInstall").attr("href", "javascript:install(" + index + ");");
+  $("#connectorInstancesList").children().remove();
+  $.each(serviceMap.installed, function(key, value) {
+    if (value["srcdir"] == item["srcdir"])
+      $("#connectorInstancesList").append("<li><span class='title'>" + value["title"] + "</span><span class='identifier'>" + value["id"] +  "</span><span class='page'><a href='" + value["uri"] + "'>Configure</a></span></li>");
+  });
 }
 
 function refreshDiary()
 {
-  $("#diaryLines").children().remove();
-  $.ajax({url:"/diary"}).success(function(data) {
+  $("#diaryEntriesList").children().remove();
+  $.ajax({ url: "/diary" }).success(function(data) {
     data.split("\n").forEach(function(item) {
       if (!item) return;
       var line = JSON.parse(item);
       if (!line || line.length == 0) return;
       var ts = line["timestamp"].toDate();
-      var diaryLine = $("#diaryTemplate").clone();
+      var diaryLine = $("#diaryEntryTemplate").clone();
       diaryLine.attr("id", "");
-      diaryLine.addClass("diaryLine");
+      diaryLine.addClass("diaryEntry");
       diaryLine.children("span").append(ts.strftime("%B %d%o at %H:%MM %P"));
       diaryLine.append(line["message"]);
-      diaryLine.appendTo("#diaryLines");
+      diaryLine.appendTo("#diaryEntriesList");
       diaryLine.show();
     });
-    $("#diarySection").animate({scrollTop:$("#diarySection").attr("scrollHeight") - $("#diarySection").height()}, 250);
+    $("#diarySection").animate({
+      scrollTop: $("#diarySection").attr("scrollHeight") - $("#diarySection").height()
+    }, 250);
   });
 }
 
 function install(i)
 {
-    document.location = "/post2install?id="+i;
+  document.location = "/post2install?id=" + i;
 }
 
 $(document).ready(function()
 {
 
   // Update Interface from Service Map
-  $.ajax({url:"/map", dataType:"json"}).success(function(data) {
+  $.ajax({ url: "/map", dataType: "json" }).success(function(data) {
     serviceMap = data;
-    // for (var key in serviceMap.installed) {
-    //   var item = serviceMap.installed[key];
-    //   $("#installedServices").append("<li><span class='title'>" + item["title"] + "</span><span class='identifier'>" + item["id"] +  "</span><span class='page'><a href='" + item["uri"] + "'>Configure</a></span></li>");
-    //   $("#services").append("<li>" + item["title"] + "</li>");
-    // }
-
-    // var appsList, connectorsList, collectionsList;
 
     serviceMap.available.forEach(function(item) {
-      console.log(item)
-
       switch (item.is)
       {
         case "app":
@@ -72,8 +71,8 @@ $(document).ready(function()
           break;
 
         case "connector":
-          $("#availList").append($("<li>" + item.title + "</li>").click(function(event) {
-            $("#availList li").removeClass("current");
+          $("#connectorsList").append($("<li>" + item.title + "</li>").click(function(event) {
+            $("#connectorsList li").removeClass("current");
             $(event.target).addClass("current");
             selectAvailable(serviceMap.available.indexOf(item));
           }));
