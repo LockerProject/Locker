81e607647e318c274037a9d77eb11c6accd0712c
Eric Jennings <ericj@loopshot.com>
2011-05-24 17:55:40 -0700

    Updated connector-local-facebook test with newer api layout

--- Connectors/Facebook/api.js
+++ Connectors/Facebook/api.js
@@ -7,58 +7,60 @@
 *
 */
 
-/*
-*
-* Data access endpoints.
-*
-*/
+var fs = require('fs'),
+    lfs = require("../../Common/node/lfs.js"),
+    dataStore = require('./dataStore.js');
 
-var lfs = require('../../Common/node/lfs.js');
-var fs = require('fs');
-var dataStore = require('./dataStore.js');
 
 module.exports = function(app, callback) {
 
 dataStore.init(function() {
-    
+
 // In adherence with the contact/* provider API
 // Returns a list of the current set of contacts (friends and followers)
 app.get('/allContacts', function(req, res) {
-    dataStore.getAllContacts(function(allContacts) {
-        res.writeHead(200, {'content-type' : 'application/json'});
-        res.end(JSON.stringify(allContacts));
+    dataStore.getPeople(function(err, allContacts) {
+        if(err) {
+        } else {
+            res.writeHead(200, {'content-type' : 'application/json'});
+            res.end(JSON.stringify(allContacts));
+        }
     });
 });
 
 // In adherence with the contact/* provider API
 // Returns a list of the current set of friends or followers
-app.get('/getCurrent/:type', function(req, res) {
-    var type = req.params.type;
-    if(type == 'friends') {
-        dataStore.getPeopleCurrent(req.params.type, function(err, profiles) {
-            if(err) {
-                console.error(err);
-            } else {
-                res.writeHead(200, {'content-type' : 'application/json'});
-                res.end(JSON.stringify(profiles));
-            }
-        });
-    }
+app.get('/getCurrent/friends', function(req, res) {
+    dataStore.getPeopleCurrent(function(err, profiles) {
+        if(err) {
+            console.error(err);
+            res.end(err);
+        } else {
+            res.writeHead(200, {'content-type' : 'application/json'});
+            res.end(JSON.stringify(profiles));
+        }
+    });
+});
+
+app.get('/getCurrent/profile', function(req, res) {
+    lfs.readObjectFromFile('profile.json', function(userInfo) {
+        res.writeHead(200, {"Content-Type":"text/json"});
+        res.end(JSON.stringify(userInfo));        
+    });
 });
 
 // In adherence with the contact/* provider API
 app.get('/getAll/:type', function(req, res) {
     var type = req.params.type;
-    if(type == 'friends') {
+    if(type == 'friends')
         getPeople({recordID:-1}, res);
-    }
 });
 
 // In adherence with the contact/* provider API
 app.get('/getSince/:type', function(req, res) {
     var type = req.params.type;
+    var query = {};
     if(type == 'friends') {
-        var query = {};
         if(req.query.recordID) {
             query.recordID = req.query.recordID;
         } else if(req.query.timeStamp) {
@@ -72,20 +74,19 @@ app.get('/getSince/:type', function(req, res) {
 });
 
 function getPeople(query, res) {
-    dataStore.getPeople(query, function(err, friends) {
-        res.writeHead(200, {'content-type' : 'application/json'});
-        res.end(JSON.stringify(friends));
+    dataStore.getPeople(query, function(err, allPeople) {
+        if(err) {
+            res.writeHead(500, {'content-type' : 'application/json'});
+            res.end(JSON.stringify(err));
+        } else {
+            res.writeHead(200, {'content-type' : 'application/json'});
+            res.end(JSON.stringify(allPeople));
+        }
     });
 }
 
-app.get('/get_profile', function(req, res) {
-    lfs.readObjectFromFile('userInfo.json', function(userInfo) {
-        res.writeHead(200, {"Content-Type":"text/json"});
-        res.end(JSON.stringify(userInfo));        
-    });
-});
-
 callback();
-
+    
 });
+
 };
