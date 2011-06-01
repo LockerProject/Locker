a40702feee01d4c4da30628ad0a9b8d006d66d4e
Simon Murtha-Smith <simon@murtha-smith.com>
2011-06-01 09:33:47 -0400

    improved Contacts Collections merging

--- Collections/Contacts/dataStore.js
+++ Collections/Contacts/dataStore.js
@@ -31,20 +31,25 @@ exports.addData = function(type, endpoint, data, callback) {
 
 exports.addTwitterData = function(relationship, twitterData, callback) {
     relationship = relationship.substring(0, relationship.length - 1);
-    var twID  = twitterData.data.id;
-    var cleanedName = cleanName(twitterData.data.name);
+    var data = twitterData.data;
+    var twID  = data.id;
+    var cleanedName = cleanName(data.name);
     var query = {'accounts.twitter.data.id':twID};
     var set = {};
-    var baseObj = {data:twitterData.data, lastUpdated:twitterData.timeStamp || new Date().getTime()};
+    var baseObj = {data:data, lastUpdated:twitterData.timeStamp || new Date().getTime()};
     baseObj[relationship] = true;
     set['accounts.twitter.$'] = baseObj;
+    if(data.name)
+        set.name = data.name;
     collection.update(query, {$set: set, $addToSet:{'_matching.cleanedNames':cleanedName}},
                         {safe:true}, function(err, doc) {
         if(!doc) {
             //match otherwise
             var or = [{'_matching.cleanedNames':cleanedName}, 
                       {'accounts.foursquare.data.contact.twitter':twitterData.data.screen_name}];
-            collection.update({$or:or}, {$push:{'accounts.twitter':baseObj}, $addToSet:{'_matching.cleanedNames':cleanedName}}, 
+            collection.update({$or:or}, {$push:{'accounts.twitter':baseObj}, 
+                                         $addToSet:{'_matching.cleanedNames':cleanedName},
+                                         $set:{'name':data.name}}, 
                         {safe:true, upsert:true}, callback);
         } else {
             callback(err, doc);
@@ -53,21 +58,32 @@ exports.addTwitterData = function(relationship, twitterData, callback) {
 }
 
 exports.addFoursquareData = function(foursquareData, callback) {
-    var foursquareID = foursquareData.data.id;
-    var cleanedName = cleanName(foursquareData.data.name);
+    var data = foursquareData.data;
+    var foursquareID = data.id;
+    var name = data.firstName + ' ' + data.lastName;
+    var cleanedName = cleanName(name);
     var query = {'accounts.foursquare.data.id':foursquareID};
     var set = {};
-    var baseObj = {data:foursquareData.data, lastUpdated:foursquareData.timeStamp || new Date().getTime()};
+    var baseObj = {data:data, lastUpdated:foursquareData.timeStamp || new Date().getTime()};
     set['accounts.foursquare.$'] = baseObj;
-    collection.update(query, {$set: set, $addToSet:{'_matching.cleanedNames':cleanedName}},
+    if(name)
+        set.name = name;
+    if(data.gender)
+        set.gender = data.gender;
+    collection.update(query, {$set: set, $addToSet:{'_matching.cleanedNames':cleanedName,
+                                                    'photos':data.photo}},
                              {safe: true}, function(err, doc) {
         if (!doc) {
             var or = [{'_matching.cleanedNames':cleanedName}];
-            if(foursquareData.data.contact.twitter)
-                or.push({'accounts.twitter.data.screen_name':foursquareData.data.contact.twitter});
-            if(foursquareData.data.contact.facebook)
-                or.push({'accounts.facebook.data.id':foursquareData.data.contact.facebook});
-            collection.update({$or:or}, {$push:{'accounts.foursquare':baseObj}, $addToSet:{'_matching.cleanedNames':cleanedName}},
+            if(data.contact.twitter)
+                or.push({'accounts.twitter.data.screen_name':data.contact.twitter});
+            if(data.contact.facebook)
+                or.push({'accounts.facebook.data.id':data.contact.facebook});
+            var set = {};
+            collection.update({$or:or}, {$push:{'accounts.foursquare':baseObj}, 
+                                         $addToSet:{'_matching.cleanedNames':cleanedName,
+                                                    'photos':data.photo},
+                                         $set:{'name':data.name, 'gender':data.gender}},
                               {safe: true, upsert: true}, callback);
         } else {
             callback(err, doc);
@@ -76,19 +92,24 @@ exports.addFoursquareData = function(foursquareData, callback) {
 }
 
 exports.addFacebookData = function(facebookData, callback) {
-    var fbID  = facebookData.data.id;
-    var cleanedName = cleanName(facebookData.data.name);
+    var data = facebookData.data;
+    var fbID  = data.id;
+    var cleanedName = cleanName(data.name);
     var query = {'accounts.facebook.data.id':fbID};
     var set = {};
-    var baseObj = {data:facebookData.data, lastUpdated:facebookData.timeStamp || new Date().getTime()};
+    var baseObj = {data:data, lastUpdated:facebookData.timeStamp || new Date().getTime()};
     set['accounts.facebook.$'] = baseObj;
+    if(data.name)
+        set.name = data.name;
     collection.update(query, {$set: set, $addToSet:{'_matching.cleanedNames':cleanedName}},
                         {safe:true}, function(err, doc) {
         if(!doc) {
             //match otherwise
             var or = [{'_matching.cleanedNames':cleanedName}, 
                       {'accounts.foursquare.data.contact.facebook':fbID}];
-            collection.update({$or:or}, {$push:{'accounts.facebook':baseObj}, $addToSet:{'_matching.cleanedNames':cleanedName}}, 
+            collection.update({$or:or}, {$push:{'accounts.facebook':baseObj}, 
+                                         $addToSet:{'_matching.cleanedNames':cleanedName},
+                                         $set:{'name':data.name}}, 
                         {safe:true, upsert:true}, callback);
         } else {
             callback(err, doc);
@@ -97,18 +118,23 @@ exports.addFacebookData = function(facebookData, callback) {
 }
 
 exports.addGoogleContactsData = function(googleContactsData, callback) {
-    var gcID  = googleContactsData.data.id;
-    var cleanedName = cleanName(googleContactsData.data.name);
+    var data = googleContactsData.data;
+    var gcID  = data.id;
+    var cleanedName = cleanName(data.name);
     var query = {'accounts.googleContacts.data.id':gcID};
     var set = {};
-    var baseObj = {data:googleContactsData.data, lastUpdated:googleContactsData.data.lastUpdated || new Date().getTime()};
+    var baseObj = {data:googleContactsData.data, lastUpdated:data.lastUpdated || new Date().getTime()};
     set['accounts.googleContacts.$'] = baseObj;
+    if(data.name)
+        set.name = data.name;
     collection.update(query, {$set: set, $addToSet:{'_matching.cleanedNames':cleanedName}},
                        {safe:true}, function(err, doc) {
         if(!doc) {
             //match otherwise
             var or = [{'_matching.cleanedNames':cleanedName}];
-            collection.update({$or:or}, {$push:{'accounts.googleContacts':baseObj}, $addToSet:{'_matching.cleanedNames':cleanedName}}, 
+            collection.update({$or:or}, {$push:{'accounts.googleContacts':baseObj}, 
+                                         $addToSet:{'_matching.cleanedNames':cleanedName},
+                                         $set:{'name':data.name}}, 
                         {safe:true, upsert:true}, callback);
         } else {
             callback(err, doc);
@@ -116,6 +142,12 @@ exports.addGoogleContactsData = function(googleContactsData, callback) {
     });
 }
 
+exports.clear = function(callback) {
+    collection.drop(callback);
+}
+
 function cleanName(name) {
-    return name;
+    if(!name || typeof name != 'string')
+        return name;
+    return name.toLowerCase();
 }
--- Collections/Contacts/sync.js
+++ Collections/Contacts/sync.js
@@ -19,30 +19,32 @@ exports.init = function(theLockerUrl, mongoCollection) {
 }
 
 exports.gatherContacts = function() {
-    // This should really be timered, triggered, something else
-    locker.providers(['contact/facebook', 'contact/twitter', 'contact/google', 'contact/foursquare'], function(services) {
-        if (!services) return;
-        services.forEach(function(svc) {
-            if(svc.provides.indexOf('contact/facebook') >= 0) {
-                exports.getContacts("facebook", "friends", svc.id, function() {
-                    console.error('facebook done!');
-                });
-            } else if(svc.provides.indexOf('contact/twitter') >= 0) {
-                exports.getContacts("twitter", "friends", svc.id, function() {
-                    exports.getContacts("twitter", "followers", svc.id, function() {
-                        console.error('twitter done!');
+    dataStore.clear(function(err) {
+        // This should really be timered, triggered, something else
+        locker.providers(['contact/facebook', 'contact/twitter', 'contact/google', 'contact/foursquare'], function(services) {
+            if (!services) return;
+            services.forEach(function(svc) {
+                if(svc.provides.indexOf('contact/facebook') >= 0) {
+                    exports.getContacts("facebook", "friends", svc.id, function() {
+                        console.error('facebook done!');
                     });
-                });
-            } else if(svc.provides.indexOf('contact/google') >= 0) {
-                exports.getContacts("google", "contacts", svc.id, function() {
-                    console.error('gcontacts done!');
-                });
-                // addContactsFromConn(svc.id, '/allContacts', 'contact/google');
-            } else if(svc.provides.indexOf('contact/foursquare') >= 0) {
-                exports.getContacts('foursquare', "friends", svc.id, function() {
-                    console.error('foursquare done!');
-                });
-            }
+                } else if(svc.provides.indexOf('contact/twitter') >= 0) {
+                    exports.getContacts("twitter", "friends", svc.id, function() {
+                        exports.getContacts("twitter", "followers", svc.id, function() {
+                            console.error('twitter done!');
+                        });
+                    });
+                } else if(svc.provides.indexOf('contact/google') >= 0) {
+                    exports.getContacts("google", "contacts", svc.id, function() {
+                        console.error('gcontacts done!');
+                    });
+                    // addContactsFromConn(svc.id, '/allContacts', 'contact/google');
+                } else if(svc.provides.indexOf('contact/foursquare') >= 0) {
+                    exports.getContacts('foursquare', "friends", svc.id, function() {
+                        console.error('foursquare done!');
+                    });
+                }
+            });
         });
     });
 }
