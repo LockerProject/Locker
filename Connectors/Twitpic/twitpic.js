d827937938c2e0f740a3dd2c014ce301dbd3e458
Jeremie Miller <jeremie@jabber.org>
2011-06-02 23:27:23 -0500

    twitpic now fetches and stores thumbnails, and knows how to go get full pics from an id on demand

--- Connectors/Twitpic/twitpic.js
+++ Connectors/Twitpic/twitpic.js
@@ -28,12 +28,14 @@ var me;
 app.set('views', __dirname);
 app.get('/', handleIndex);
 
+var handle=false;
 function handleIndex(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     lfs.readObjectFromFile('auth.json', function(newAuth) {
         if(newAuth.handle)
         {
             res.end(fs.readFileSync(__dirname + '/ui/index.html'));
+            handle=newAuth.handle;
         }else{
             res.end(fs.readFileSync(__dirname + '/ui/init.html'));
         }
@@ -41,7 +43,7 @@ function handleIndex(req, res) {
 }
 
 app.get('/init', function(req, res) {
-    var handle = req.param('handle');
+    handle = req.param('handle');
     if(!handle || handle.length == 0) {
         res.writeHead(400);
         res.end('whats the handle yo?');
@@ -49,17 +51,49 @@ app.get('/init', function(req, res) {
     }
     console.log("initializing to "+handle);
     fs.writeFileSync('auth.json', JSON.stringify({handle:handle}));
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end("saved "+handle+", <a href='./'>continue</a>");
+    fs.mkdir("thumbs-"+handle,0755,function(err){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end("saved "+handle+", <a href='./'>continue</a>");        
+    });
 });
 
 app.get('/sync', function(req, res) {
-    lfs.readObjectFromFile('auth.json', function(auth) {
-        tpage(auth.handle,1);
-    });
+    if(!handle)
+    {
+        res.writeHead(404);
+        res.end("missing handle");
+    }
+    tpage(1);
     res.writeHead(200);
     res.end("kthxbye");
 });
+
+app.get('/full/:pic', function(req, res) {
+    if(!req.params.pic)
+    {
+        res.writeHead(404);
+        res.end("missing id");
+    }
+    var url = "http://twitpic.com/"+req.params.pic+"/full";
+    request.get({uri:url}, function(err, resp, body) {
+        if(err)
+        {
+            res.writeHead(404);
+            res.end("twitpic failed: "+err);
+            return;
+        }
+        var parts = body.split('http://s3.amazonaws.com/twitpic/photos/full/');
+        if(parts.length != 2)
+        {
+            res.writeHead(404);
+            res.end("couldn't find image file");
+            return;
+        }
+        var tails = parts[1].split('"');
+        res.redirect('http://s3.amazonaws.com/twitpic/photos/full/'+tails[0]);
+    });    
+});
+
 
 /*
     <ul id="user-photos"> 
@@ -84,7 +118,7 @@ app.get('/sync', function(req, res) {
 
 var fullQ = [];
 var idCache = {};
-function tpage(handle,num)
+function tpage(num)
 {
     var url = "http://twitpic.com/photos/"+handle+"?page="+num;
     console.log("fetching page "+url);
@@ -111,7 +145,6 @@ function tpage(handle,num)
 //            ["",">\n    \t\t\t<a href=","/3e58mc","><img alt=","This is what programming books do me, especially ones that mention tdd. Yeh I need a shave."," src=","http://s3.amazonaws.com/twitpic/photos/thumb/205157604.jpg?AWSAccessKeyId=AKIAJF3XCCKACR3QDMOA&Expires=1306907764&Signature=4aYz74qZFfrKZvsf5rpxiKUIBJs%3D"," style=","width: 150px; height: 150px;"," /></a>\n    \t\t\t\n    \t\t\t    \t\t</div>\n    \t\t<div class=","user-tweet",">\n    \t\t\t<p>This is what programming books do me, especially ones that mention tdd. Yeh I need a shave.</p>\n    \t\t\t<p class=","tweet-meta",">\n    \t\t\t\t174 days ago from <a class=","nav-link"," target=","_blank"," href=","http://www.blackberry.com",">BlackBerry®</a> &bull; viewed 45 times\n    \t\t\t</p>\n    \t\t\t\n    \t\t\t    \t\t</div>\n    \t</li>\n    \t\t\t\t    \t    \t<li>\n    \t\t<div class=",""]
 
             var parts = inner[i].split('"');
-            console.log("PARTS\t\t"+JSON.stringify(parts));
             if(parts.length < 6) continue;
             var metas = inner[i].split('<p class="tweet-meta">');
             if(metas.length <= 1) continue;
@@ -123,14 +156,38 @@ function tpage(handle,num)
                 console.log("dup detected, probably at last page, bail");
                 return;
             }
-            var pic = {id:id, txt:parts[3], thumb:parts[5], meta:meta};
-            fullQ.push(id);
+            var thumb = 'thumbs-'+handle+'/' + id + '.jpg';
+            var pic = {id:id, txt:parts[3], thumb:thumb, meta:meta};
+            fullQ.push({id:id, url:parts[5], file:thumb});
             idCache[id]=pic;
             console.log(JSON.stringify(pic));
             lfs.appendObjectsToFile("pics-"+handle+".json",[pic]);
         }
+        thumbQ(); // kick off in case not running
         num++;
-        setTimeout(function(){tpage(handle,num);},2000); // next page in 2sec
+        setTimeout(function(){tpage(num);},2000); // next page in 2sec
+    });
+}
+
+var tqBG=false; // is tq fetcher running
+function thumbQ(go)
+{
+    if(!go && tqBG) return;
+    tqBG=true;
+    var thumb = fullQ.pop();
+    if(!thumb)
+    {
+        tqBG=false;
+        return;
+    }
+    request.get({uri:thumb.url, encoding: 'binary'}, function(err, resp, body) {
+        if(err)
+            console.error(err + " for " + JSON.stringify(thumb));
+        else
+        {
+            fs.writeFileSync(thumb.file, body, 'binary');
+            setTimeout(function(){thumbQ(true);},2000);
+        }
     });
 }
 
