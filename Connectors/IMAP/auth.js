d0f54220409923fa37c833158d4977eddcc666b3
Eric Jennings <ericj@loopshot.com>
2011-06-03 14:56:31 -0700

    IMAP connector refactored in JS

---
+++ Connectors/IMAP/auth.js
@@ -0,0 +1,89 @@
+/*
+*
+* Copyright (C) 2011, The Locker Project
+* All rights reserved.
+*
+* Please see the LICENSE file for more information.
+*
+*/
+
+var request = require('request'),
+    lfs = require('../../Common/node/lfs.js'),
+    fs = require('fs');
+
+var completedCallback, me;
+
+exports.auth = {};
+
+exports.authAndRun = function(app, onCompletedCallback) {
+    me = app.meData;
+
+    if(exports.isAuthed()) {
+        onCompletedCallback();
+        return;
+    }
+    completedCallback = onCompletedCallback;
+    app.get('/go', go);
+    app.post('/saveAuth', saveAuth);
+};
+
+exports.isAuthed = function() {
+    try {
+        if(!exports.auth) {
+            exports.auth = {};
+        }
+        
+        if((exports.auth.username && exports.auth.password && exports.auth.host && exports.auth.port)) {
+            return true;
+        }
+    
+        console.error('isAuthed.reading in from', process.cwd());
+        var authData = JSON.parse(fs.readFileSync('auth.json', 'utf-8'));
+        console.error('isAuthed.read and parsed ' + authData.username);
+        
+        if(authData.hasOwnProperty('username') && 
+           authData.hasOwnProperty('password') && 
+           authData.hasOwnProperty('host') && 
+           authData.hasOwnProperty('port')) {
+            exports.auth = authData;
+            return true;
+        }
+    } catch (E) {
+        console.error(E);
+    }
+    return false;
+};
+
+function go(req, res) {
+    if(!(exports.auth.username && exports.auth.password && exports.auth.host && exports.auth.port)) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end("<html>Enter your IMAP server info that will be used to sync your data" + 
+                "<form method='post' action='saveAuth'>" + 
+                    "Username: <input name='username'><br>" +
+                    "Password: <input type='password' name='password'><br>" +
+                    "Host: <input name='host'><br>" +
+                    "Port: <input name='port' value='993'><br>" +
+                    "Host uses SSL: <input type='checkbox' name='secure' checked='checked' value='true'><br>" +
+                    "<input type='submit' value='Save'>" +
+                "</form></html>");
+    } else {
+        sys.debug('redirecting to ' + me.uri);
+        res.redirect(me.uri);
+    }
+}
+
+function saveAuth(req, res) {
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    if(!req.body.username || !req.body.password || !req.body.host  || !req.body.port) {
+        res.end("missing field(s)?");
+        return;
+    }
+    exports.auth.username = req.param('username');
+    exports.auth.password = req.param('password');
+    exports.auth.host = req.param('host');
+    exports.auth.port = req.param('port');
+    exports.auth.secure = req.param('secure');
+    lfs.writeObjectToFile('auth.json', exports.auth);
+    completedCallback(exports.auth);
+    res.redirect(me.uri);
+}
---
+++ Connectors/IMAP/sync-api.js
@@ -0,0 +1,59 @@
+/*
+*
+* Copyright (C) 2011, The Locker Project
+* All rights reserved.
+*
+* Please see the LICENSE file for more information.
+*
+*/
+
+var fs = require('fs'),
+    sync = require('./sync'),
+    locker = require('../../Common/node/locker.js');
+    
+var app, auth;
+
+module.exports = function(theApp) {
+    app = theApp;
+    app.get('/', index);
+    this.authComplete = authComplete;
+    return this;
+};
+
+function authComplete(theAuth, mongoCollections) {
+    auth = theAuth;
+    sync.init(auth, mongoCollections);
+
+    app.get('/friends', friends);
+    app.get('/messages', messages);
+    sync.eventEmitter.on('message/imap', function(eventObj) {
+        locker.event('message/imap', eventObj);
+    });
+}
+
+function index(req, res) {
+    if(!(auth && auth.username && auth.password && auth.imaphost && auth.imapport))
+        res.redirect(app.meData.uri + 'go');
+    else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end("<html>Found valid authentication, sync up your <a href='messages'>mail messages</a></html>");
+    }
+}
+
+function friends(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    sync.syncFriends(function(err, repeatAfter, diaryEntry) {
+        locker.diary(diaryEntry);
+        locker.at('/friends', repeatAfter);
+        res.end(JSON.stringify({success: "done fetching friends"}));
+    });
+}
+
+function messages(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    sync.syncMessages(function(err, repeatAfter, diaryEntry) {
+        locker.diary(diaryEntry);
+        locker.at('/messages', repeatAfter);
+        res.end(JSON.stringify({success: "done fetching messages"}));
+    });
+}
