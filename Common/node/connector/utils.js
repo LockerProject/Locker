2439eafea060c4ab2395f01b0b648f406d34cab0
Chris Burkhart <christide@christide.com>
2011-06-08 16:39:53 -0700

    Convert existing knownIDs objects into arrays

--- Common/node/connector/utils.js
+++ Common/node/connector/utils.js
@@ -4,6 +4,15 @@ exports.checkDeletedIDs = function(knownIDs, returnedIDs) {
             repeatedIDs = {},
             removedIDs = [];
 
+        // hack to handle pre-existing objects instead of arrays, should probably have a way to wipe out /Me directories
+        // after making changes that break existing data
+        //
+        if (!(Array.isArray(knownIDs))) {
+            var tempIDs = knownIDs;
+            knownIDs = [];
+            for (var i in tempIDs)
+                knownIDs.push(i);
+        }
         returnedIDs.forEach(function(id) {
             if (knownIDs.indexOf(id) !== -1)
                 repeatedIDs[id] = 1;
