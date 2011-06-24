05412ca97f3337f836740ce162971944145bd5ee
ctide <christide@christide.com>
2011-06-24 12:22:06 -0700

    Make links and contacts collections always listen to events, even if they haven't been spawned yet.

---
+++ Collections/Links/migrations/1308942327128_add_events.js
@@ -0,0 +1,13 @@
+module.exports = function(dir) {
+    process.chdir(dir);
+    var path = require('path');
+    var fs = require('fs');
+    
+    var me = fs.readFileSync('me.json');
+    me = JSON.parse(me);
+    
+    me['events'] = JSON.parse('[["link/facebook","/events"],["link/twitter","/events"]]');
+    
+    fs.writeFileSync('me.json', JSON.stringify(me), 'ascii');
+    return true;
+};
