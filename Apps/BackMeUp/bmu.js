942a3b1be966d15bb7205ee0d305d629a23681e0
Jeremie Miller <jeremie@jabber.org>
2011-06-26 23:40:22 -0500

    was broken due to common lib change, fixed now

--- Apps/BackMeUp/bmu.js
+++ Apps/BackMeUp/bmu.js
@@ -13,7 +13,6 @@ var express = require('express'),connect = require('connect');
 var app = express.createServer(connect.bodyParser());
 var locker = require('../../Common/node/locker.js');
 var lfs = require('../../Common/node/lfs.js');
-var lconfig = require('../../Common/node/lconfig.js');
 var exec = require('child_process').exec;
 var util = require('util');
 var request = require('request');
@@ -27,11 +26,11 @@ function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    locker.map(function(map){
+    locker.map(function(error,map){
         // find dropbox
         for(var id in map.installed)
         {
-            if(map.installed[id].provides && map.installed[id].provides.indexOf("store/dropbox") >= 0)
+            if(map.installed[id].provides && map.installed[id].provides.indexOf("store/s3") >= 0)
             {
                 dbox=id;
                 res.end("<!--"+id+"-->Place a new Me_bu.tgz of your Me/ data in dropbox by running a <a href='./backup'>backup</a> right now (may take a while).");
@@ -47,7 +46,7 @@ app.get('/backup',function(req, res) {
         'Content-Type': 'text/html'
     });
     res.write("<p>hold on, backing up in progress...\n");
-    var child = exec('rm -f /tmp/Me_bu.tgz && tar -cvzf /tmp/Me_bu.tgz ./' + lconfig.me, {cwd: '../../'}, function (error, stdout, stderr) {
+    var child = exec('rm -f /tmp/Me_bu.tgz && tar -cvzf /tmp/Me_bu.tgz ./Me', {cwd: '../../'}, function (error, stdout, stderr) {
         if (error !== null) {
             res.end("failed: "+error+" stdout: "+stdout+" stderr: "+stderr);
             return;
