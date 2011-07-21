55bc71ff12b12bcc4bd3b298e3d860c22472cb8c
Thomas Muldowney <temas@sparecog.com>
2011-07-21 17:39:45 -0500

    Check that the twitter structure is valid before using it to generate a link

--- Collections/Links/dataStore.js
+++ Collections/Links/dataStore.js
@@ -24,7 +24,7 @@ exports.addEvent = function(data, callback) {
     var type = data.via;
     if (type.indexOf('facebook') !== -1) {
         exports.addLink("facebook", data.obj.data, data.obj.data.url, callback);
-    } else if (type.indexOf('twitter') !== -1) {
+    } else if (type.indexOf('twitter') !== -1 && data.obj && data.obj.hasOwnProperty("status") && data.obj.status.hasOwnProperty("entities") && data.obj.status.entities.hasOwnProperty("urls")) {
         exports.addLink("twitter", data.obj.status, data.obj.status.entities.urls[0].expanded_url || data.obj.status.entities.urls[0].url, callback);
     }
 }
