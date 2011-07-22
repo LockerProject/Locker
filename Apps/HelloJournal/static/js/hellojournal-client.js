ffc121295c0423ef75f8e8325b1323cc820d89d0
Tom Longson (nym) <tomlong@gmail.com>
2011-07-22 15:50:57 -0700

    Adding HelloPhotos demo app. Untested / unstable.

--- Apps/HelloJournal/static/js/hellojournal-client.js
+++ Apps/HelloJournal/static/js/hellojournal-client.js
@@ -3,24 +3,17 @@ var log = function(msg) { if (console && console.log) console.debug(msg); };
 
 /**
  * Reload the display (get journal, render them)
- * @property offset {Integer} Optional Where in the journal collection you want to start.
- * @property limit {Integer} Optional The number of journal you want returned.
- * @property useJSON {Boolean} Optional Display raw JSON instead of the journal's name.
  */
-function reload(offset, limit, useJSON) {
-    // set the params if not specified
-    var offset = offset || 0; 
-    var limit = limit || 100;
-    var useJSON = useJSON || false;
+function reload() {
+    var journalList = $("select:first");
 
     var getJournalsCB = function(journals) {
 	// find the unordered list in our document to append to
-        var journalList = $("select:first");
 
 	// clear the list
 	journalList.html('');
 	
-	// populate the list with our journal
+	// populate the li`ast with our journal
 	if (journals.length == 0) journalList.append("<option>Sorry, no journal(s) found!</option>");
         for (var i in journals) {
 	    var journal = journals[i];
@@ -33,6 +26,11 @@ function reload(offset, limit, useJSON) {
 	    var optHTML = '<option id="' + journal._id + '" class="journal">'+journalHTML+'</div>';
 	    journalList.append(optHTML);
 	}
+        journalList.attr("disabled", false);
+    };
+
+    var getJournalCB = function(journal) {
+        
     };
 
     $.getJSON(
@@ -40,6 +38,8 @@ function reload(offset, limit, useJSON) {
 	{}, 
 	getJournalsCB
     );
+
+    
 }
 
 /* jQuery syntactic sugar for onDomReady */
