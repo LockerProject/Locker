34635434a7d17fcce42bf959f4a3c9cc8fe8a5ae
ctide <christide@christide.com>
2011-08-08 17:59:10 -0700

    Clean out commented out test code

--- tests/lsyncmanager-test-local.js
+++ tests/lsyncmanager-test-local.js
@@ -135,59 +135,6 @@ vows.describe("Synclet Manager").addBatch({
                 }
             }
         }
-    // },
-    // "Migrates services that need it during the install" : {
-    //     topic: [],
-    //     "changing their version" : function(topic) {
-    //         assert.include(serviceManager.serviceMap().installed, "migration-test");
-    //         assert.isTrue(serviceManager.isInstalled("migration-test"));
-    //         assert.notEqual(serviceManager.serviceMap().installed['migration-test'], undefined);
-    //         assert.notEqual(serviceManager.serviceMap().installed['migration-test'].version, undefined);
-    //         assert.equal(serviceManager.serviceMap().installed['migration-test'].version, 1308079085972);
-    //     },
-    //     "and running the migration successfully" : function(topic) {
-    //         var me = JSON.parse(fs.readFileSync(process.cwd() + "/" + lconfig.me + "/migration-test/me.json", 'ascii'));
-    //         assert.notEqual(me.mongoCollections, undefined);
-    //         assert.equal(me.mongoCollections[0], 'new_collection');
-    //     }
     }
-// }).addBatch({
-//     "Spawning a service": {
-//         topic : function() {
-//             request({url:lconfig.lockerBase + '/Me/echo-config/'}, this.callback);
-//         },
-//         "passes the externalBase with the process info": function(err, resp, body) {
-//             var json = JSON.parse(body);
-//             assert.equal(json.externalBase, lconfig.externalBase + '/Me/echo-config/');
-//         }
-//     }
-// }).addBatch({
-//     "Uninstalling services " : {
-//         topic: function() {
-//             var that = this;
-//             request({uri:lconfig.lockerBase + '/core/tests/uninstall', json:{serviceId:'disabletest'}, method: 'POST'}, function() {
-//                 path.exists(lconfig.me + "/disabletest", function(exists) {
-//                     if (exists) {
-//                         that.callback("directory still exists");
-//                     } else {
-//                         that.callback(false, true);
-//                     }
-//                 })
-//             });
-//         },
-//         "deletes them FOREVER" : function(err, resp) {
-//             assert.isNull(err);
-//             assert.isTrue(resp);
-//         },
-//         "and deletes" : {
-//             topic : function() {
-//                 mongo.collections.thing1.count(this.callback);
-//             },
-//             "mongo collections" : function(err, doc) {
-//                 assert.isNull(err);
-//                 assert.equal(doc, 0);
-//             }
-//         }
-//     }
 }).export(module);
 
