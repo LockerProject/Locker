f3c68be029d43c6757cdbb16f29ec782ac0e75f4
Chris Hannam <bassdread@gmail.com>
2011-08-18 13:58:45 +0100

    working map example and data rendered on page

--- Apps/FoursquareMayors/static/js/foursquaremayors-client.js
+++ Apps/FoursquareMayors/static/js/foursquaremayors-client.js
@@ -7,6 +7,40 @@ var log = function(msg) { if (console && console.log) console.debug(msg); }
  * @property limit {Integer} Optional The number of contacts you want returned.
  * @property useJSON {Boolean} Optional Display raw JSON instead of the contact's name.
  */
+
+var map;
+
+function add_marker(map, lat, loong, html, open, icon) {
+      var myLatlng = new google.maps.LatLng(lat,loong);
+      var marker = new google.maps.Marker({
+        position: myLatlng, 
+        map: map,
+		icon: icon
+      });
+      
+      var infowindow = new google.maps.InfoWindow({
+            content: html
+      });
+      
+      google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+      });
+      if (open == true){
+	infowindow.open(map,marker);
+      }
+}
+
+function initialize() {
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var myOptions = {
+      zoom: 8,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+        myOptions);
+}
+
 function reload(offset, limit, useJSON) {
     // set the params if not specified
     var offset = offset || 0; 
@@ -14,11 +48,32 @@ function reload(offset, limit, useJSON) {
     var useJSON = useJSON || false;
 
     var getContactsCB = function(contacts) {
-        alert('meh');
+	// find the unordered list in our document to append to
+        var contactsList = $("#main ul");
+
+	// clear the list
+	contactsList.html('');
+	
+	// populate the list with our contacts
+	if (contacts.length == 0) contactsList.append("<li>Sorry, no contacts found!</li>");
+        for (var i in contacts) {
+
+	    contact = contacts[i];
+	    
+	    log(contact);
+	    if (useJSON) {
+		contactHTML = "<pre>"+ JSON.stringify(contact['accounts']['foursquare'][0]['data']['mayorships'], null, 2) +"</pre>";
+	    } else {
+		// get the contact name, but use the first email address if no name exists
+		contactHTML = contact.name || contact.emails[0].value;
+	    }
+	    liHTML = '<li id="' + contact._id + '" class="contact"><span class="basic-data">'+contactHTML+'</span></div>';
+	    contactsList.append(liHTML);
+	}
     };
 
     $.getJSON(
-	'/',
+	'/query/getContact',
 	{'offset':offset, 'limit':limit}, 
 	getContactsCB
     );
