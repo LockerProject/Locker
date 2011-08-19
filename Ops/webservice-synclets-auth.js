d54f37d1711c62a6a1675d5a69bf62afe4911aa1
ctide <christide@christide.com>
2011-08-19 11:55:16 -0700

    add some error handling around auth

--- Ops/webservice-synclets-auth.js
+++ Ops/webservice-synclets-auth.js
@@ -56,88 +56,100 @@ module.exports = function(locker) {
 };
 
 function handleOAuth2 (code, options, res) {
-    var newUrl = options.endPoint + '/access_token' +
-                    '?client_id=' + apiKeys[options.provider].appKey +
-                    '&client_secret=' + apiKeys[options.provider].appSecret +
-                    '&grant_type=' + options.grantType +
-                    '&redirect_uri=' + host + options.redirectURI +
-                    '&code=' + code;
-    request.get({url:newUrl}, function(err, resp, body) {
-        auth = {};
-        if (options.accessTokenResponse == 'json') {
-            auth.accessToken = JSON.parse(body).access_token;
-        } else {
-            auth.accessToken = querystring.parse(body).access_token;
-        }
-        if (options.provider === 'github') {
-            request.get({url:"https://github.com/api/v2/json/user/show?access_token=" + auth.accessToken}, function(err, resp, body) {
-                try {
-                    var resp = JSON.parse(body);
-                    auth.username = resp.user.login;
-                    installSynclet(options.provider, auth);
-                } catch (e) {
-                    console.error('Failed to auth github - ' + body);
-                }
-            });
-        } else {
-            installSynclet(options.provider, auth);
-        }
-        res.end("<script type='text/javascript'> window.close(); </script>");
-    });
+    try {
+        var newUrl = options.endPoint + '/access_token' +
+                        '?client_id=' + apiKeys[options.provider].appKey +
+                        '&client_secret=' + apiKeys[options.provider].appSecret +
+                        '&grant_type=' + options.grantType +
+                        '&redirect_uri=' + host + options.redirectURI +
+                        '&code=' + code;
+        request.get({url:newUrl}, function(err, resp, body) {
+            auth = {};
+            if (options.accessTokenResponse == 'json') {
+                auth.accessToken = JSON.parse(body).access_token;
+            } else {
+                auth.accessToken = querystring.parse(body).access_token;
+            }
+            if (options.provider === 'github') {
+                request.get({url:"https://github.com/api/v2/json/user/show?access_token=" + auth.accessToken}, function(err, resp, body) {
+                    try {
+                        var resp = JSON.parse(body);
+                        auth.username = resp.user.login;
+                        installSynclet(options.provider, auth);
+                    } catch (e) {
+                        console.error('Failed to auth github - ' + body);
+                    }
+                });
+            } else {
+                installSynclet(options.provider, auth);
+            }
+            res.end("<script type='text/javascript'> window.close(); </script>");
+        });
+    } catch (E) {
+        res.end('failed to authenticate against service - ' + E);
+    }
 }
 
 function handleOAuth2Post (code, options, res) {
-    var postData = {grant_type:options.grantType,
-              code:code,
-              client_id:apiKeys[options.provider].appKey,
-              client_secret:apiKeys[options.provider].appSecret,
-              redirect_uri:host + options.redirectURI};
-    // request won't ever return here.  no idea why.
-    //
-    // request({method: 'post', uri :options.endPoint, body: querystring.stringify(postData)}, function(err, resp, body) {
-    //     console.error(err);
-    //     console.error(resp);
-    //     console.error(body);
-    //     auth = {};
-    //     auth.token = JSON.parse(data);
-    //     installSynclet(options.provider, auth);
-    //     res.end("<script type='text/javascript'>if (window.opener) { window.opener.location.reload(true); } window.close(); </script>");
-    // });
+    try {
+        var postData = {grant_type:options.grantType,
+                  code:code,
+                  client_id:apiKeys[options.provider].appKey,
+                  client_secret:apiKeys[options.provider].appSecret,
+                  redirect_uri:host + options.redirectURI};
+        // request won't ever return here.  no idea why.
+        //
+        // request({method: 'post', uri :options.endPoint, body: querystring.stringify(postData)}, function(err, resp, body) {
+        //     console.error(err);
+        //     console.error(resp);
+        //     console.error(body);
+        //     auth = {};
+        //     auth.token = JSON.parse(data);
+        //     installSynclet(options.provider, auth);
+        //     res.end("<script type='text/javascript'>if (window.opener) { window.opener.location.reload(true); } window.close(); </script>");
+        // });
 
-    var httpOptions = {
-        host: 'accounts.google.com',
-        port: 443,
-        path: '/o/oauth2/token',
-        method: 'POST',
-        headers: {'Content-Type':'application/x-www-form-urlencoded'}
-    };
-    var httpsReq = https.request(httpOptions, function(httpsRes) {
-        httpsRes.on('data', function(data) {
-            auth = {};
-            auth.clientID = apiKeys[options.provider].appKey;
-            auth.clientSecret = apiKeys[options.provider].appSecret;
-            auth.token = JSON.parse(data);
-            installSynclet(options.provider, auth);
-            res.end("<script type='text/javascript'> window.close(); </script>");
+        var httpOptions = {
+            host: 'accounts.google.com',
+            port: 443,
+            path: '/o/oauth2/token',
+            method: 'POST',
+            headers: {'Content-Type':'application/x-www-form-urlencoded'}
+        };
+        var httpsReq = https.request(httpOptions, function(httpsRes) {
+            httpsRes.on('data', function(data) {
+                auth = {};
+                auth.clientID = apiKeys[options.provider].appKey;
+                auth.clientSecret = apiKeys[options.provider].appSecret;
+                auth.token = JSON.parse(data);
+                installSynclet(options.provider, auth);
+                res.end("<script type='text/javascript'> window.close(); </script>");
+            });
         });
-    });
-    httpsReq.write(querystring.stringify(postData));
-    httpsReq.on('error', function(e) {
-        callback(e, null);
-    });
-    httpsReq.end();
+        httpsReq.write(querystring.stringify(postData));
+        httpsReq.on('error', function(e) {
+            callback(e, null);
+        });
+        httpsReq.end();
+    } catch (E) {
+        res.end('failed to authenticate against service - ' + E);
+    }
 }
 
 function handleTwitter (req, res) {
-    require('../Connectors/Twitter/twitter_client')(apiKeys.twitter.appKey, apiKeys.twitter.appSecret, host + "auth/twitter/auth")
-        .getAccessToken(req, res, function(err, newToken) {
-            var auth = {};
-            auth.consumerKey = apiKeys.twitter.appKey;
-            auth.consumerSecret = apiKeys.twitter.appSecret;
-            auth.token = newToken;
-            installSynclet("twitter", auth);
-            res.end("<script type='text/javascript'> window.close(); </script>");
-        });
+    try {
+        require('../Connectors/Twitter/twitter_client')(apiKeys.twitter.appKey, apiKeys.twitter.appSecret, host + "auth/twitter/auth")
+            .getAccessToken(req, res, function(err, newToken) {
+                var auth = {};
+                auth.consumerKey = apiKeys.twitter.appKey;
+                auth.consumerSecret = apiKeys.twitter.appSecret;
+                auth.token = newToken;
+                installSynclet("twitter", auth);
+                res.end("<script type='text/javascript'> window.close(); </script>");
+            });
+    } catch (E) {
+        res.end('failed to authenticate against service - ' + E);
+    }
 }
 
 function installSynclet (provider, auth) {
