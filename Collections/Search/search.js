9db84df0d843f32db312c3481e79333dc4c5b86a
Eric Jennings <ericj@loopshot.com>
2011-08-24 17:40:18 -0700

    Begin adding the async logic code to enrich search results

--- Collections/Search/search.js
+++ Collections/Search/search.js
@@ -11,10 +11,13 @@ var fs = require('fs'),
     locker = require('../../Common/node/locker.js');
     
 var lsearch = require("../../Common/node/lsearch");
+var lutil = require("../../Common/node/lutil");
 
 var lockerInfo;
 var express = require('express'),
     connect = require('connect');
+var request = require('request');
+var async = require('async');
 var app = express.createServer(connect.bodyParser());
 
 app.set('views', __dirname);
@@ -29,46 +32,48 @@ function handleError(type, action, id, error) {
 
 app.post("/events", function(req, res) {
     if (req.headers["content-type"] === "application/json" && req.body) {
+        console.error('About to index the following:');
+        console.error(req.body);   
         if (req.body.type === "contact/full") {
             if (req.body.action === "new" || req.body.action === "update") {
-                lsearch.indexType("contact", req.body.obj.data, function(err, time) {
-                    if (err) { handleError(req.body.type, req.body.action, req.body._id, err); }
+                lsearch.indexType("contacts", req.body.obj.data, function(err, time) {
+                    if (err) { handleError(req.body.type, req.body.action, req.body.obj.data._id, err); }
                 });
             } else if (req.body.action === "delete") {
                 lsearch.deleteDocument(req.body.obj.data._id, function(err, time, docsDeleted) {
-                    if (err) { handleError(req.body.type, req.body.action, req.body._id, err); }
-                    console.log('Received delete event for contact/full id: ' + req.body._id);
+                    if (err) { handleError(req.body.type, req.body.action, req.body.obj.data._id, err); }
+                    console.log('Received delete event for contact/full id: ' + req.body.obj.data._id);
                 });
             }
             res.end();
         } else if (req.body.type === "status/twitter") {
             if (req.body.action === "new" || req.body.action === "update") {
                 lsearch.indexType(req.body.type, req.body.obj.status, function(err, time) {
-                    if (err) { handleError(req.body.type, req.body.action, req.body._id, err); }
+                    if (err) { handleError(req.body.type, req.body.action, req.body.obj.data._id, err); }
                 });
             } else if (req.body.action === "delete") {
                 lsearch.deleteDocument(req.body.obj.data._id, function(err, time, docsDeleted) {
-                    if (err) { handleError(req.body.type, req.body.action, req.body._id, err); }
+                    if (err) { handleError(req.body.type, req.body.action, req.body.obj.data._id, err); }
                 });
             }
             res.end();
         } else if (req.body.type) {
             if (req.body.action === "new" || req.body.action === "update") {
                 lsearch.indexType(req.body.type, req.body.obj, function(err, time) {
-                    if (err) { handleError(req.body.type, req.body.action, req.body._id, err); }
+                    if (err) { handleError(req.body.type, req.body.action, req.body.obj.data._id, err); }
                 });
             } else if (req.body.action === "delete") {
                 lsearch.deleteDocument(req.body.obj.data._id, function(err, time, docsDeleted) {
-                    if (err) { handleError(req.body.type, req.body.action, req.body._id, err); }
+                    if (err) { handleError(req.body.type, req.body.action, req.body.obj.data._id, err); }
                 });
             }
             res.end();
         } else {
-            console.log("Unexpected event: " + req.body.type + " and " + req.body.action);
+            console.error("Unexpected event: " + req.body.type + " and " + req.body.action);
             res.end();
         }
     } else {
-        console.log("Unexpected event or not json " + req.headers["content-type"]);
+        console.error("Unexpected event or not json " + req.headers["content-type"]);
         res.end();
     }
 });
@@ -79,9 +84,9 @@ app.post("/index", function(req, res) {
         res.end("Invalid arguments");
         return;
     }
-    
+    var value = {};
     try {
-        var value = JSON.parse(req.body.value);
+        value = JSON.parse(req.body.value);
     } catch(E) {
         res.writeHead(500);
         res.end("invalid json in value");
@@ -100,22 +105,36 @@ app.post("/index", function(req, res) {
 app.get("/query", function(req, res) {
     var q = req.param("q");
     var type = req.param("type");
-    
+
     if (!q || q === '*') {
         res.writeHead(400);
-        res.end("Must supply at least a query");
+        res.end("Please supply valid query string");
         return;
     }
+
     function sendResults(err, results, queryTime) {
         if (err) {
             res.writeHead(500);
             res.end("Error querying: " + err);
             return;
         }
-        var data = {};
-        data.hits = results;
-        data.took = queryTime;
-        res.send(data);
+
+        enrichResultsWithFullObjects(results, function(err, richResults) {
+            var data = {};
+            data.took = queryTime;
+        
+            if (err) {
+                data.error = err;
+                data.hits = [];
+                res.end(JSON.stringify(data));
+            }
+        
+            data.error = null;
+            data.hits = richResults;
+            data.total = richResults.length;
+            res.end(JSON.stringify(data));
+        });
+        
     }
     if (type) {
         lsearch.queryType(type, q, {}, sendResults);
@@ -123,6 +142,81 @@ app.get("/query", function(req, res) {
         lsearch.queryAll(q, {}, sendResults);
     }
 });
+
+function cullAndSortResults(results, callback) {
+    async.sortBy(results, function(item, sortByCb) {
+        // we concatenate the score to the type, and we use the reciprocal of the score so the sort has the best scores at the top
+        sortByCb(null, item._type + (1/item.score).toFixed(3));
+    },
+    function(err, results) {
+       callback(null, results); 
+    });
+}
+
+function makeEnrichedRequest(url, item, callback) {
+    request.get({uri:url}, function(err, res, body) {
+        if (err) {
+            console.error('Error when attempting to enrich search results: ' + err);
+            callback(err);
+            return;
+        } 
+        if (res.statusCode >= 400) {
+            var error = 'Received a ' + res.statusCode + ' when attempting to enrich search results';
+            console.error(error);
+            callback(error);
+            return;
+        }
+
+        item.fullobject = JSON.parse(body);
+        callback(null);
+    });
+}
+
+function enrichResultsWithFullObjects(results, callback) {
+    // fetch full objects of results
+    async.waterfall([
+        function(waterfallCb) {
+            cullAndSortResults(results, function(err, results) {
+                waterfallCb(err, results);
+            });
+        },
+        function(results, waterfallCb) {
+            async.forEach(results, 
+                function(item, forEachCb) {
+                    var splitType = item._type.split('/');
+                    if (splitType.length === 1) {
+                        var url = lockerInfo.lockerUrl + '/Me/' + splitType[0] + '/' + item._id;
+                        makeEnrichedRequest(url, item, forEachCb);
+                    } else {
+                        locker.providers(item._type, function(err, providers) {
+                            async.forEach(providers,
+                                function(provider, providersForEachCb) {
+                                    console.error('GOOOT HEEERRE');
+                                    // query /Me/:syncletId/:type/:id
+                                    var url = lockerInfo.lockerUrl + '/Me/' + provider.id + '/' + splitType[0] + '/' + item._id;
+                                    console.log(url);
+                                    makeEnrichedRequest(url, item, providersForEachCb);
+                                },
+                                function(err) {
+                                    forEachCb(null);
+                                }
+                            );
+                        });
+                    }
+                }, 
+                function(err) {
+                    waterfallCb(err, results);
+                }
+            ); 
+        }
+    ],
+    function(err, results) {        
+        if (err) {  
+            callback('Error when attempting to sort and enrich search results: ' + err, []);
+        }
+        callback(null, results);
+    });
+}
 
 // Process the startup JSON object
 process.stdin.resume();
@@ -133,7 +227,7 @@ process.stdin.on('data', function(data) {
         data = allData.substr(0, allData.indexOf("\n"));
         lockerInfo = JSON.parse(data);
         locker.initClient(lockerInfo);
-        if (!lockerInfo || !lockerInfo['workingDirectory']) {
+        if (!lockerInfo || !lockerInfo.workingDirectory) {
             process.stderr.write('Was not passed valid startup information.'+data+'\n');
             process.exit(1);
         }
