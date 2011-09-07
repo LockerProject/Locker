9e67048ee183fd43af1791a76838827fe4dbdcf5
Thomas Muldowney <temas@sparecog.com>
2011-09-07 13:57:16 -0500

    Wraps the file output logger with a basic function to change the max listeners

--- Common/node/logger.js
+++ Common/node/logger.js
@@ -30,9 +30,21 @@ ModuleConsoleLogger.prototype.log = function(level, msg, meta, callback) {
     this.doLog(level, msg, meta, callback);
 }
 
+// This is a copy of the normal file logger so that we can work around a winston bug that spews warnings on too many listeners
+function FileBugLogger(options) {
+	winston.transports.File.call(this, options);
+	this.name = "fileBug";
+}
+util.inherits(FileBugLogger, winston.transports.File);
+FileBugLogger.prototype.name = "fileBug";
+FileBugLogger.prototype._realOpen = FileBugLogger.prototype.open;
+FileBugLogger.prototype.open = function(cb) {
+	this._realOpen(cb);
+	if (this.stream) this.stream.setMaxListeners(0);
+};
 
 var transports = [new ModuleConsoleLogger({colorize:true,timestamp:true})];
 if (lconfig.logFile) {
-    transports.push(new (winston.transports.File)({filename:path.join("Logs", lconfig.logFile)}));
+    transports.push(new FileBugLogger({filename:path.join("Logs", lconfig.logFile)}));
 }
 exports["logger"] = new (winston.Logger)({"transports":transports});
