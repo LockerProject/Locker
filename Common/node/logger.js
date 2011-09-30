e71e8e032d5231020b61a7507e2b05229e690551
Simon Murtha-Smith <simon@murtha-smith.com>
2011-09-30 11:26:01 -0700

    added timestamp to log files, fixes #223

--- Common/node/logger.js
+++ Common/node/logger.js
@@ -45,7 +45,7 @@ FileBugLogger.prototype.open = function(cb) {
 
 var transports = [new ModuleConsoleLogger({colorize:true,timestamp:true})];
 if (lconfig.logFile) {
-    var fileLogger = new FileBugLogger({filename:path.join("Logs", lconfig.logFile)});
+    var fileLogger = new FileBugLogger({filename:path.join("Logs", lconfig.logFile), timestamp:true});
     fileLogger.on("open", function() {
         var realWrite = fileLogger.stream.write;
         fileLogger.stream.write = function(data) {
