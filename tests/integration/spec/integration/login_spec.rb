89acdf72872537d216ceef56898d753edca774bd
ctide <christide@christide.com>
2011-11-02 13:11:16 -0700

    fix test to properly switch between viewers

--- tests/integration/spec/integration/login_spec.rb
+++ tests/integration/spec/integration/login_spec.rb
@@ -1,4 +1,4 @@
-require 'spec/spec_helper.rb'
+require File.join(File.expand_path(File.dirname(__FILE__)), '../spec_helper.rb')
 
 #describe 'home page', :type => :request do
 describe 'dashboard' do
@@ -7,6 +7,16 @@ describe 'dashboard' do
     page.should have_css('#contacts.selected')
     within_frame 'appFrame' do
       page.should have_content('Jeremie')
+    end
+    page.execute_script("$('#photos').click()")
+    page.should have_css('#photos.selected')
+    within_frame 'appFrame' do
+      page.should have_content('Photos')
+    end
+    page.execute_script("$('#links').click()")
+    page.should have_css('#links.selected')
+    within_frame 'appFrame' do
+      page.should have_content('Links')
     end
   end
 end
