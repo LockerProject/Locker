6d4fb93719b69ead22fd6010f771cd08a6ec2fe2
Eric Jennings <ericj@loopshot.com>
2011-11-10 19:10:34 -0800

    Add front-end test for helloplaces

---
+++ tests/integration/spec/integration/helloplaces_spec.rb
@@ -0,0 +1,15 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), '../spec_helper.rb')
+
+#describe 'home page', :type => :request do
+
+#describe 'helloplaces' do
+#  it 'allows people to view all scrolled locations' do
+#    visit '/dashboard/#places'
+#    page.should have_css('#places.selected')
+#      within_frame 'appFrame' do
+#      page.should have_css('#placeslist')
+#      page.execute_script("$('.placelink:first-of-type').click()")
+#      page.should have_css('#mapcanvas img')
+#    end
+#  end
+#end
