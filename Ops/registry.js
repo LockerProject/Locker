94770c298fa8806ec035b5c244b415172cceca1e
Jeremie Miller <jeremie@jabber.org>
2011-11-14 19:47:11 -0600

    using semver to compare versions, better install/update/sync logic

--- Ops/registry.js
+++ Ops/registry.js
@@ -29,11 +29,17 @@ var npm = require('npm');
 var fs = require('fs');
 var path = require('path');
 var async = require('async');
+var request = require('request');
+var semver = require('semver');
+var lutil = require('lutil');
 var lconfig;
 var installed = {};
+var regIndex = {};
+var syncInterval = 3600000;
+var syncTimer;
+var regBase = 'http://registry.singly.com/';
 
-var conf = {registry:'http://registry.singly.com/npm'};
-
+// make sure stuff is ready/setup locally, load registry, start sync check, etc
 exports.init = function(config, callback) {
     lconfig = config;
     try {
@@ -41,10 +47,19 @@ exports.init = function(config, callback) {
     } catch(E) {}
     process.chdir(lconfig.me);
     loadInstalled(function(){
-        npm.load(conf, function(err) {
-            process.chdir(lconfig.lockerDir);
+        npm.load({registry:regBase+'npm'}, function(err) {
             if(err) console.error(err);
-            callback(installed);
+            fs.readFile('registry.json', 'utf8', function(err, reg){
+                try {
+                    if(reg) regIndex = JSON.parse(reg);
+                }catch(E){
+                    console.error("couldn't parse registry.json: "+E);
+                }
+                syncTimer = setInterval(exports.sync, syncInterval);
+                exports.sync();
+                process.chdir(lconfig.lockerDir);
+                callback(installed);
+            });            
         });        
     });
 };
@@ -53,31 +68,71 @@ exports.init = function(config, callback) {
 function loadInstalled(callback)
 {
     var files = fs.readdirSync("node_modules");
-    async.forEach(files, function(file, cb){
-        var ppath = path.join('./node_modules/', file, 'package.json');
+    async.forEach(files, function(item, cb){
+        var ppath = path.join('./node_modules/', item, 'package.json');
         fs.stat(ppath, function(err, stat){
             if(err || !stat || !stat.isFile()) return cb();
-            fs.readFile(ppath, function(err, data){
-                if(err || !data) return cb();
-                try{
-                    var js = JSON.parse(data);
-                    if(!js.name) throw new Error("invalid package");
-                    installed[js.name] = js;
-                }catch(E){
-                    console.error("couldn't parse "+ppath+": "+E);
-                }
-                cb();
-            });
+            loadPackage(item, cb);
         });
     }, callback);
 }
 
-exports.install = function(arg, callback) {
-    if(!arg || !arg.name) return callback("missing package name");
-    npm.commands.install([arg.name], callback);
+// load an individual package
+function loadPackage(name, callback)
+{
+    fs.readFile(path.join(lconfig.me, 'node_modules', name, 'package.json'), 'utf8', function(err, data){
+        if(err || !data) return callback(err);
+        try{
+            var js = JSON.parse(data);
+            if(!js.name) throw new Error("invalid package");
+            installed[js.name] = js;                    
+        }catch(E){
+            console.error("couldn't parse "+name+"'s package.json: "+E);
+            return callback();
+        }
+        request.get({uri:lconfig.lockerBase+'/map/upsert?manifest=Me/node_modules/'+name+'/package.json'}, callback);
+    });
+}
+
+// background sync process to fetch/maintain the full package list
+exports.sync = function()
+{
+    var startkey = 0;
+    // get the newest
+    Object.keys(regIndex).forEach(function(k){
+        var mod = new Date(regIndex[k].time.modified).getTime();
+        if(mod > startkey) startkey = mod;
+    });
+    // look for updated packages newer than the last we've seen
+    var u = regBase+'npm/-/all/since?stale=update_after&startkey='+startkey;
+    console.log("registry update from "+u);
+    request.get({uri:u, json:true}, function(err, resp, body){
+        if(err || !body || Object.keys(body).length === 0) return;
+        // replace in-mem representation
+        Object.keys(body).forEach(function(k){
+            regIndex[k] = body[k];
+            // if installed and autoupdated and newer, do it!
+            if(installed[k] && body[k].update == 'auto' && semver.lt(installed[k].dist-tags.latest, body[k].dist-tags.latest))
+            {
+                console.log("auto-updating "+k+" to "+body[k].dist-tags.latest);
+                exports.update({name:k}, function(){}); // lazy
+            }
+        });
+        // cache to disk lazily
+        lutil.atomicWriteFileSync(path.join(lconfig.me, 'registry.json'), JSON.stringify(regIndex));
+    });
 };
 
+// npm wrappers
+exports.install = function(arg, callback) {
+    if(!arg || !arg.name) return callback("missing package name");
+    npm.commands.install([arg.name], function(){
+        loadPackage(arg.name, callback); // once installed, load
+    });
+};
 exports.update = function(arg, callback) {
     if(!arg || !arg.name) return callback("missing package name");
-    npm.commands.update([arg.name], callback);
+    npm.commands.update([arg.name], function(){
+        loadPackage(arg.name, callback); // once updated, re-load
+    });
 };
