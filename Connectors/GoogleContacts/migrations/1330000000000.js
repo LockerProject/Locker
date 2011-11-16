b4a40d2abd3ca6fe7c68ddc6ec28044fd0d54ed8
Simon Murtha-Smith <simon@murtha-smith.com>
2011-11-16 15:32:02 -0800

    update to the gcontacts migration

--- Connectors/GoogleContacts/migrations/1330000000000.js
+++ Connectors/GoogleContacts/migrations/1330000000000.js
@@ -3,7 +3,11 @@ module.exports = function(dir) {
     var fs = require('fs');
     
     var me = JSON.parse(fs.readFileSync('me.json'));
-    me.config = null;
+    me.config = {
+        startIndex: 1, // start over from the beginning
+        lastUpdate: 1,
+        nextRun: -1    // run right now because the contacts collection will update and we don't have the raw data yet
+    };
     fs.writeFileSync('me.json', JSON.stringify(me, null, 4));
     
     return true;
