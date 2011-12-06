c2b24d5a16129a3f76cee743d45e1b389cb5bf29
Jeremie Miller <jeremie@jabber.org>
2011-12-06 11:33:48 -0600

    socket hang up, I believe this is a source, using request that catches that stuff, and I think fixes #450

--- Common/node/locker.js
+++ Common/node/locker.js
@@ -54,12 +54,6 @@ exports.diary = function(message, level) {
     });
 };
 
-exports.makeRequest = function(httpOpts, body, callback) {
-    var req = http.request(httpOpts, callback);
-    req.write(body);
-    req.end();
-};
-
 exports.map = function(callback) {
     request.get({url:exports.lockerBase + "/map"}, function(error, res, body) {
         callback(error, body ? JSON.parse(body) : undefined);
