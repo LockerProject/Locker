780225066f85cdd722fe8ae282c315ff94882660
ctide <christide@christide.com>
2011-12-08 18:41:55 -0800

    update tests

--- tests/integration/spec/integration/dashboard_spec.rb
+++ tests/integration/spec/integration/dashboard_spec.rb
@@ -4,37 +4,37 @@ require File.join(File.expand_path(File.dirname(__FILE__)), '../spec_helper.rb')
 describe 'dashboard' do
   it 'allows people to switch between 4 views' do
     visit '/'
-    page.should have_css('.iframeLink.orange[data-id="contactsviewer"]')
+    page.should have_css('.iframeLink.blue[data-id="contactsviewer"]')
     within_frame 'appFrame' do
       page.should have_content('Jeremie')
     end
     page.execute_script("$('.iframeLink[data-id=\"photosv09\"]').click()")
-    page.should have_css('.iframeLink.orange[data-id="photosv09"]')
+    page.should have_css('.iframeLink.blue[data-id="photosv09"]')
     within_frame 'appFrame' do
       page.should have_content('Photos')
     end
     page.execute_script("$('.iframeLink[data-id=\"linkalatte\"]').click()")
-    page.should have_css('.iframeLink.orange[data-id="linkalatte"]')
+    page.should have_css('.iframeLink.blue[data-id="linkalatte"]')
     within_frame 'appFrame' do
       page.should have_content('Links')
     end
     page.execute_script("$('.iframeLink[data-id=\"helloplaces\"]').click()")
-    page.should have_css('.iframeLink.orange[data-id="helloplaces"]')
+    page.should have_css('.iframeLink.blue[data-id="helloplaces"]')
     within_frame 'appFrame' do
       page.should have_content('Places')
     end
   end
 
-  it 'should allow access to api explorer' do
-    visit '/'
-    sleep 1
-    page.execute_script("$('.iframeLink[data-id=\"devdocs\"]').click()")
-    within_frame 'appFrame' do
-      page.should have_content('Make your own viewer!')
-      click_on 'API Explorer'
-      page.should have_content('API Explorer')
-    end
-  end
+  #it 'should allow access to api explorer' do
+    #visit '/'
+    #sleep 1
+    #page.execute_script("$('.iframeLink[data-id=\"devdocs\"]').click()")
+    #within_frame 'appFrame' do
+      #page.should have_content('Make your own viewer!')
+      #click_on 'API Explorer'
+      #page.should have_content('API Explorer')
+    #end
+  #end
 
   it "should allow the user to view all of their apps" do
     visit '/'
