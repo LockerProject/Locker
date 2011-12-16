e6f1d4d1def67c7f84d50a5fa39d752288099f85
Jeremie Miller <jeremie@jabber.org>
2011-12-16 09:22:11 -0600

    another fix, hopefully for good

--- Collections/Search/sync.js
+++ Collections/Search/sync.js
@@ -43,34 +43,37 @@ function gatherFromUrl(svcId, callback) {
     var url = path.join("Me", svcId, "?all=true&stream=true");
     url = lconfig.lockerBase + "/" + url;
     logger.info("updating from "+url);
-    var total = 0;
     var req = request.get({uri:url}, function(err){
         if(err) logger.error(err);
     });
+    var total = 0;
+    var q = async.queue(function(chunk, cb){
+        if(chunk == "") return cb();
+        total++;
+        try{
+            exports.add(svcId, JSON.parse(chunk), cb);
+        }catch(E){
+            logger.error("got "+E+" processing "+chunk);
+            return cb();
+        }
+    },1);
     var buff = "";
-    var q = async.queue(function(data, cbq){
+    req.on("data",function(data){
         buff += data.toString();
         var chunks = buff.split('\n');
         buff = chunks.pop(); // if was end \n, == '', if mid-stream it'll be a not-yet-complete chunk of json
-        async.forEachSeries(chunks, function(chunk, cb){
-            total++;
-            try{
-                exports.add(svcId, JSON.parse(chunk), cb);
-            }catch(E){
-                logger.error("got "+E+" processing "+chunk);
-                return cb();
-            }
-        }, function(err){
-            if(err) logger.error("got error "+err);
-            cbq();
-        });
-    },1);
-    req.on("data",q.push);
+        chunks.forEach(q.push);
+    });
+    var ended = false;
     q.drain = function(){
+        if(!ended) return; // drain can be called many times, we only care when it's after data is done coming in
         logger.info("indexed "+total+" items from "+svcId);
         callback();
     };
-    req.on("end",function(){q.push("")}); // this triggers the drain if there was no data, GOTCHA
+    req.on("end",function(){
+        ended = true;
+        q.push(""); // this triggers the drain if there was no data, GOTCHA
+    });
 }
 
 exports.add = function(service, data, callback){
