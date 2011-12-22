1a7f55a295b0f2dfac82a50212907d1010fcdce6
Simon Murtha-Smith <simon@murtha-smith.com>
2011-12-22 11:58:25 -0500

    fixes bug with google oauth 2.0

--- Connectors/GooglePlus/auth.js
+++ Connectors/GooglePlus/auth.js
@@ -2,5 +2,5 @@ module.exports = {
     endPoint : "https://accounts.google.com/o/oauth2/token",
     grantType : "authorization_code",
     handler : {oauth2 : 'POST'},
-    authUrl : "https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/plus.me&response_type=code&access_type=offline"
+    authUrl : "https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/plus.me&response_type=code&access_type=offline&approval_prompt=force"
 }
