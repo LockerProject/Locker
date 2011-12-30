4b75e8d3079d0d7c8d672e4c38524d00f9beb8fb
Jeremie Miller <jeremie@jabber.org>
2011-12-30 04:38:19 -0600

    all the interesting raw and summary data synclets for a BodyMedia FIT device, and exploring some new synclet/oauth dev patterns

---
+++ Connectors/BodyMedia/lib.js
@@ -0,0 +1,16 @@
+
+exports.genericSync = function(type, pather, arrayer) {
+    return function(pi, cb) {
+        var OAlib = require('oauth').OAuth;
+        var OA = new OAlib(null, null, pi.auth.consumerKey, pi.auth.consumerSecret, '1.0', null, 'HMAC-SHA1', null, {'Accept': '*/*', 'Connection': 'close'});
+        var path = pather(pi);
+        if(!path) return cb(null, {config:pi.config, data:{}}); // nothing to do
+        var url = 'http://api.bodymedia.com/v2/json/'+path+'?api_key='+pi.auth.consumerKey;
+        OA.get(url, pi.auth.token, pi.auth.tokenSecret, function(err, body){
+            try{ var js = JSON.parse(body); }catch(E){ return cb(err); }
+            var data = {};
+            data[type] = arrayer(pi, js);
+            cb(err, {config:pi.config, data: data});
+        });
+    };
+};
