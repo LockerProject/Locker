987d46b181d43a20acb374b4a95048f062a458cc
ctide <christide@christide.com>
2012-01-03 16:56:39 -0800

    remove debugging

--- Apps/dashboardv3/migrations/1325636562997_delete_registry_json.js
+++ Apps/dashboardv3/migrations/1325636562997_delete_registry_json.js
@@ -1,5 +1,4 @@
 module.exports = function(dir) {
-    console.dir(require('path').join(dir, '..', 'registry.json'));
     require('fs').unlink(require('path').join(dir, '..', 'registry.json'));
     return true;
 };
