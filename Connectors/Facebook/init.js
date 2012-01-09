2969b998cdb44f99ba9c03fbfdf40945b2ad88b1
Jeremie Miller <jeremie@jabber.org>
2012-01-09 13:35:12 -0600

    facebook connector converted and published

--- Connectors/Facebook/init.js
+++
@@ -1,8 +0,0 @@
-// this is a common pattern many connectors use, it processes the startup data and eventfully loads the auth.js, sync-api.js, etc
-require.paths.push(__dirname);
-require('connector/client').init({"oauth2" :
-    {"provider" : "Facebook",
-     "endPoint" : "https://graph.facebook.com/oauth",
-     "linkToCreate" : "http://www.facebook.com/developers/createapp.php",
-     "extraParams" : "scope=email,offline_access,read_stream,user_photos,friends_photos,publish_stream,user_photo_video_tags"}
-});
