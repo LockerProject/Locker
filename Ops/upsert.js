79dea7933368cff5245d37988f71d0734a18efd0
Jeremie Miller <jeremie@jabber.org>
2012-01-11 12:29:18 -0600

    fix upsert, tested n works

--- Ops/upsert.js
+++ Ops/upsert.js
@@ -4,7 +4,7 @@ var request = require('request');
 var lconfig = require(path.join(__dirname, "../Common/node/lconfig.js"));
 lconfig.load(path.join(__dirname, "../Config/config.json"));
 
-if(!path.exists("./package.json"))
+if(!path.existsSync("./package.json"))
 {
     console.error("missing ./package.json?");
     process.exit(1);
