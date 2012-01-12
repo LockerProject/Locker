235b586f9e53722ea94b5616f012a46fae441c76
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-01-12 10:10:36 -0800

    Made accumulators less global, fixed mongoId.

--- Connectors/LastFM/info.js
+++ Connectors/LastFM/info.js
@@ -10,9 +10,9 @@
 var path   = require('path')
   , lastfm = require(path.join(__dirname, 'lib.js'));
 
-var info = [];
-
 exports.sync = function (processInfo, cb) {
+    var info = [];
+
     lastfm.getInfo(processInfo
                  , function (me) {
                        info = [me];
