829ef9c62c5c450f23311a8125aa4ceabbe77724
Jeremie Miller <jeremie@jabber.org>
2012-01-13 14:34:02 -0600

    trying to fix tests

--- tests/migrations/1315435666981_test.js
+++ tests/migrations/1315435666981_test.js
@@ -2,10 +2,10 @@ var path = require('path')
   , fs = require('fs')
   ;
 
-module.exports = function(lconfig) {
+module.exports = function(lconfig, cb) {
     var state = fs.readFileSync(path.join(lconfig.lockerDir, lconfig.me, "state.json"));
     state = JSON.parse(state);
     state.migrated = true;
     fs.writeFileSync(path.join(lconfig.lockerDir, lconfig.me, "state.json"), JSON.stringify(state));
-    return true;
+    return cb(true);
 };
