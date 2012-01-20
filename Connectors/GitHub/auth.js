5a8b37c5aea3dfc9061ac1f74d04008d8e5abdfb
Jeremie Miller <jeremie@jabber.org>
2012-01-20 17:32:48 -0600

    make sure we ask for the right scope

--- Connectors/GitHub/auth.js
+++ Connectors/GitHub/auth.js
@@ -3,7 +3,7 @@ var request = require('request');
 module.exports = {
     endPoint : 'https://github.com/login/oauth',
     handler : {oauth2 : 'GET'},
-    authUrl : 'https://github.com/login/oauth/authorize?&response_type=code'
+    authUrl : 'https://github.com/login/oauth/authorize?scope=repo&response_type=code'
 };
 
 module.exports.authComplete = function(auth, callback) {
