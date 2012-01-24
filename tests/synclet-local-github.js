bb8acb5a22911963ce41eb8b03b0369a75999ef4
Jeremie Miller <jeremie@jabber.org>
2012-01-23 18:12:39 -0600

    github synclet test clean

--- tests/synclet-local-github.js
+++ tests/synclet-local-github.js
@@ -18,70 +18,15 @@ var mePath = '/Data/github-1';
 var pinfo = JSON.parse(fs.readFileSync(__dirname + mePath + '/me.json'));
 
 suite.next().suite.addBatch({
-    "Can get users" : {
-        topic: function() {
-            fakeweb.allowNetConnect = false;
-            fakeweb.registerUri({
-                uri : 'https://github.com/api/v2/json/user/show/ctide/followers',
-                file : __dirname + '/fixtures/github/followers.json' });
-            fakeweb.registerUri({
-                uri : 'https://github.com/api/v2/json/user/show/fourk',
-                file : __dirname + '/fixtures/github/fourk.json' });
-            fakeweb.registerUri({
-                uri : 'https://github.com/api/v2/json/user/show/smurthas',
-                file : __dirname + '/fixtures/github/smurthas.json' });
-            fakeweb.registerUri({
-                uri : 'https://github.com/api/v2/json/user/show/ctide/following',
-                file : __dirname + '/fixtures/github/following.json' });
-            fakeweb.registerUri({
-                uri : 'https://github.com/api/v2/json/user/show/wmw',
-                file : __dirname + '/fixtures/github/wmw.json' });
-            users.sync(pinfo, this.callback)
-        },
-        "successfully" : function(err, response) {
-            assert.equal(response.data['followers'][0].obj.company, 'Focus.com');
-            assert.equal(response.data['followers'][1].obj.id, 399496);
-            assert.equal(response.data['following'][0].obj.login, 'wmw');
-        }
-    }
-})
-.addBatch({
-    "Can get repos" : {
-        topic: function() {
-            fakeweb.allowNetConnect = false;
-            fakeweb.registerUri({
-                uri : 'https://github.com/api/v2/json/repos/show/ctide',
-                file : __dirname + '/fixtures/github/repos.json' });
-            fakeweb.registerUri({
-                uri :'https://github.com/api/v2/json/repos/show/ctide/arenarecapslibrary/watchers',
-                file : __dirname + '/fixtures/github/arenarecapslibrary_watchers.json'});
-            fakeweb.registerUri({
-                uri :'https://github.com/api/v2/json/repos/show/ctide/WoWCombatLogParser/watchers',
-                file : __dirname + '/fixtures/github/WoWCombatLogParser_watchers.json'});
-            fakeweb.registerUri({
-                uri : 'https://github.com/api/v2/json/user/show/ctide',
-                file : __dirname + '/fixtures/github/ctide.json' });
-            fakeweb.registerUri({
-                uri : 'https://api.github.com/repos/ctide/arenarecapslibrary/git/trees/HEAD?recursive=1',
-                contentType:"application/json",
-                body: JSON.parse(fs.readFileSync(__dirname + '/fixtures/github/arenarecapslibrary_tree.json')) });
-            fakeweb.registerUri({
-                uri : 'https://api.github.com/repos/ctide/WoWCombatLogParser/git/trees/HEAD?recursive=1',
-                contentType:"application/json",
-                body: JSON.parse(fs.readFileSync(__dirname + '/fixtures/github/arenarecapslibrary_tree.json')) });
-            repos.sync(pinfo, this.callback) },
-        "successfully" : function(err, response) {
-            assert.equal(response.data.repo[0].name, 'arenarecapslibrary');
-            assert.equal(response.data.repo[0].watchers[0], 'ctide');
-            assert.equal(response.data.repo[0].tree[0].path, 'README');
-        }
-    }
-}).addBatch({
     "Can get profile" : {
         topic: function() {
+            fakeweb.allowNetConnect = false;
+            fakeweb.registerUri({
+                uri : 'https://api.github.com:443/user?access_token=abc',
+                file : __dirname + '/fixtures/github/ctide.json' });
             profile.sync(pinfo, this.callback) },
         "successfully" : function(err, response) {
-            assert.equal(response.data.profile[0].obj.login, 'ctide');
+            assert.equal(response.data.profile[0].login, 'ctide');
         }
     }
 })
