2abba5f921f3dd14ecfebc4a66ec52809d421b32
Simon Murtha-Smith <simon@murtha-smith.com>
2012-01-26 15:38:29 -0800

    just delete Me dirs from github and resync everything. some fixes to lsyncmanager and _lockerd, including adding a postStartup migration option that runs as the very last thing in the startup process, after everything is up and alive.

--- migrations/0000000000005.js
+++ migrations/0000000000005.js
@@ -4,22 +4,29 @@
 */
 var fs = require("fs");
 var path = require("path");
-var async = require("async");
-var logger = require("logger");
 
+var async = require("async");
+var request = require("request");
+var wrench = require("wrench");
+
+var logger = require("logger");
 var lutil = require("lutil");
 
 module.exports.preServices = function(config, callback) {
     logger.info("Migrating to publish cleanup");
-    reMe(config, function(a, b) {
-        console.error("DEBUG: a", a);
-        console.error("DEBUG: b", b);
-        callback(true);
+    var meDir = path.join(config.lockerDir, config.me);
+    reMe(meDir, function(err) {
+        console.error('DEBUG: done reMe');
+        if(err) return callback(false);
+        clearCached(meDir, function(err) {
+            console.error('DEBUG: done clearCache');
+            if(err) return callback(false);
+            callback(true);
+        })
     });
 }
 
-function reMe(config, callback) {
-    var meDir = path.join(config.lockerDir, config.me);
+function reMe(meDir, callback) {
     logger.info("Checking " + meDir + " for old github app directories");
     fs.readdir(meDir, function(err, dirs) {
         if (err) return callback(err);
@@ -27,7 +34,7 @@ function reMe(config, callback) {
             return fs.statSync(path.join(meDir, dir)).isDirectory() && dir.indexOf('mongodata') === -1;
         });
         async.forEachSeries(dirs, function(dir, cb) {
-            cleanMe(path.join(config.me, dir), cb);
+            cleanMe(path.join(meDir, dir), cb);
         }, callback);
     });
 }
@@ -35,33 +42,29 @@ function reMe(config, callback) {
 // update a me.json file in the dir
 function cleanMe(dir, callback) {
     logger.info("Checking " + dir);
-    
+
     readMe(dir, function(err, me) {
-        console.error("DEBUG: err", err);
-        console.error("DEBUG: me", me);
         // not all dirs in Me have a me.json (node_modules, mongodata, etc)
         if(err || !me) return callback();
-        
+
         // only do this for services from github
         if(!(me.srcdir && me.srcdir.indexOf('Me/github/') === 0)) return callback();
-        
-        logger.info("Migrating " + dir);
-        
-        // get the github repo name from the end of the path
-        var repo = me.srcdir.substring(10);
-        
-        // this is logic from the github repos.js synclet to normalize the repo name into a handle
-        var handle = repo.replace('/', '-').toLowerCase();
-        
-        // these all need to be the same
-        me.handle = me.id = me.provider = handle;
-        writeMeSync(dir, me);
-        
-        // rename the dir to match the new id
-        var ldir = dir.toLowerCase();
-        fs.renameSync(dir, ldir);
-        
-        //done!
+
+        logger.info("Removing me dir github repo app in " + dir);
+        wrench.rmdirRecursive(dir, callback);
+    });
+}
+
+// clear out the "cache" field from the github me.json config. Will force repos to pull fresh.
+function clearCached(meDir, callback) {
+    logger.info('looking for github data...');
+    var ghDir = path.join(meDir, 'github');
+    readMe(ghDir, function(err, me) {
+        if(err) return callback(err);
+        logger.info('github data found, clearing cache...');
+        delete me.config.cached;
+        writeMeSync(ghDir, me);
+        logger.info('cleared github config cache...');
         callback();
     });
 }
