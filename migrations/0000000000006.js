471fc27beb633e10ef105af51b47b91c045c5604
Simon Murtha-Smith <simon@murtha-smith.com>
2012-01-27 09:41:35 -0800

    ignore err if already running repos synclet

--- migrations/0000000000006.js
+++ migrations/0000000000006.js
@@ -12,7 +12,7 @@ module.exports.postStartup = function(config, callback) {
     fs.stat(path.join(config.lockerDir, config.me, 'github'), function(err, stat) {
         if(!stat.isDirectory()) return callback(true);
         request.get({uri:config.lockerBase + '/Me/github/run?id=repos'}, function(err, resp, body) {
-            if(err || resp.statusCode !== 200) {
+            if(err || resp.statusCode !== 200 && body !== "already running") {
                 if (resp.statusCode !== 200) logger.error("couldn't pull GitHub repos, response from connector: " + body);
                 if (err) logger.error(new Error(err).stack);
                 return callback(false);
