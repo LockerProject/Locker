b2839b67b4192cc2c424f988a45487af95b39329
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-01-31 14:38:46 -0800

    Properly populate default values in settings.

--- Apps/dashboardv3/static/js/settings.js
+++ Apps/dashboardv3/static/js/settings.js
@@ -1,10 +1,16 @@
+var info = {};
+
+function setUserGlobals(data) {
+    info = data;
+}
+
 $(function () {
     $(".connect-button-link").click(function (e) {
         e.preventDefault();
         showHiddenConnectors();
     });
 
-    //copied from dashboard.js
+    // copied from connect.js
     $('body').delegate('.oauthLink','click', function (e) {
         var options = "width=" + $(this).data('width') + ",height=" + $(this).data('height') + ",status=no,scrollbars=no,resizable=no";
         var popup = window.open($(this).attr('href'), "account", options);
@@ -15,6 +21,10 @@ $(function () {
     if ($('.sidenav-items.synclets .installed', window.parent.document).length > 0) {
         showAllConnectors();
     }
+
+    $('input[name=accountholder-name]').val(info.name);
+    $('input[name=email]').val(info.email);
+    $('input[name=avi-url]').val(info.imageUrl);
 
     showAllConnectors();
 });
