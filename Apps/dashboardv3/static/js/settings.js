3683f254f7303cab805897d8737d44d3cb62b07d
Eric Jennings <ericj@loopshot.com>
2012-02-01 18:31:15 -0800

    Update API token handling

--- Apps/dashboardv3/static/js/settings.js
+++ Apps/dashboardv3/static/js/settings.js
@@ -9,7 +9,27 @@ $(function () {
         e.preventDefault();
         showHiddenConnectors();
     });
-
+    
+    $("#showToken").click(function(e) {
+      $("#apiToken").val(info.apiToken);
+    });
+    
+    $("#resetToken").click(function(e) {
+      $.ajax({
+        url: info.externalHost + '/users/me/resetApiToken',
+        crossDomain: true,
+        type: 'POST'
+      })
+      .done(function(data) {
+            $("#apiToken").val(info.apiToken);
+            console.log(data, status);
+      })
+      .fail(function(data, status, three) {
+            console.error('Uhoh: ' + data);
+            console.error(data);
+      });
+    });
+    
     // copied from connect.js
     $('body').delegate('.oauthLink','click', function (e) {
         var options = "width=" + $(this).data('width') + ",height=" + $(this).data('height') + ",status=no,scrollbars=no,resizable=no";
