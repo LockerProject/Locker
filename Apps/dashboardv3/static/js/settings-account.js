e72612f7d707ef53f349edc3d2ffd4052da832b2
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-02-02 23:38:13 -0800

    Tightened up formatting considerably.

--- Apps/dashboardv3/static/js/settings-account.js
+++ Apps/dashboardv3/static/js/settings-account.js
@@ -4,37 +4,20 @@ $(function () {
     $('input[name=avi_url]').val(info.imageUrl);
 
     var validate = $('#settings-account').validate({
-        debug : true,
         rules : {
-            username : {
-                required  : true,
-                minlength : 2
-            },
-            email : {
-                required : true,
-                email    : true
-            },
-            old_password : {
-                minlength : 6
-            },
-            new_password : {
-                minlength : 6
-            }
+            username : {required  : true,
+                        minlength : 2},
+            email : {required : true,
+                     email    : true},
+            old_password : {minlength : 6},
+            new_password : {minlength : 6}
         },
         messages : {
-            username : {
-                required  : "We need to know what to call you, hoss.",
-                minlength : "At least 2 characters are necessary."
-            },
-            email : {
-                required : "Your email address is also how you sign in. Required!"
-            },
-            old_password : {
-                minlength : "At least 6 characters are necessary."
-            },
-            new_password : {
-                minlength : "At least 6 characters are necessary."
-            }
+            username     : {required  : "We need to know what to call you, hoss.",
+                            minlength : "At least 2 characters are necessary."},
+            email        : {required : "Your email address is also how you sign in. Required!"},
+            old_password : {minlength : "At least 6 characters are necessary."},
+            new_password : {minlength : "At least 6 characters are necessary."}
         },
         submitHandler : function (form) {
             var post = {lockerName   : $(username).val(),
