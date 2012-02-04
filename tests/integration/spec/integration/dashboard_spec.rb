1189eb9a1efc027e8c29cff2470299c50cf9043d
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-02-03 21:29:42 -0800

    Fixed up Account Settings integration tests.
    Also squared up other tests to match master. It seems like a bad idea to
    merge to master until all of these tests pass. Down to only 1 failing!

--- tests/integration/spec/integration/dashboard_spec.rb
+++ tests/integration/spec/integration/dashboard_spec.rb
@@ -2,6 +2,13 @@ require File.join(File.expand_path(File.dirname(__FILE__)), '../spec_helper.rb')
 
 #describe 'home page', :type => :request do
 describe 'dashboard' do
+  it 'allows people to see the connect page' do
+    visit '/'
+    within_frame 'appFrame' do
+      page.should have_content("Nobody Selected")
+    end
+  end
+
   it 'should allow people to access the develop interface' do
     visit '/'
     click_link 'DEVELOP'
@@ -19,26 +26,16 @@ describe 'dashboard' do
     end
   end
 
-  it "should allow the user to view all of their apps" do
-    visit '/'
-    click_link 'see all'
-    within_frame 'appFrame' do
-      page.should have_content('Hello Links, by Singly, Inc.')
-      page.execute_script("$('li[data-id=\"hellolinks\"]').click()")
-      page.should have_content('This is example of how easy it is to load your links using HTML and jQuery')
-    end
-  end
-
   it "should allow account holders to change their settings" do
     visit '/'
     click_link 'Account Settings'
     page.should have_content('ACCOUNT SETTINGS')
   end
 
-  it "should allow account holders to connect to services (by default)" do
+  it "should default to connecting to services (by default)" do
     visit '/'
     click_link 'Account Settings'
     page.should have_content('ACCOUNT SETTINGS')
-    page.should have_css('.iframeLink.blue[data-id="connections"]')
+    page.should have_css('.iframeLink.blue[data-id="Settings-Connections"]')
   end
 end
