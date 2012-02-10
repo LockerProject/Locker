c4659f9e8aa055a22687f737ae92842b3af9e032
Kristján Pétursson <kristjan@singly.com>
2012-02-10 15:55:04 -0800

    Further refactor /develop
    - Even simpler selected-section selection
      We can target to match the URL hash instead of having to tag everything with
      its own ID or class. This'll make it much easier to add more in the future.
    - Encapsulate page behavior in Locker.Develop and Locker.Develop.BuildAnApp
      objects
    - Move prettifying JS out of develop-buildapp template
    - Fix header and navigation vanishing after connecting GitHub
    - Remove locker.js include from templates
      Forrest added it to header_partial; reincluding it just blows away additions.
      That itself is kind of unfortunate. jQuery.extend() might be the answer there.
    - Make "Build an App" header the same height as the rest
      It was jarring for the nav to move around. The long paragraph that was in the
      header is now a description directly below the nav.
    - Bit of spacing in the GitHub connect section

--- Apps/dashboardv3/static/js/develop.js
+++ Apps/dashboardv3/static/js/develop.js
@@ -1,43 +1,37 @@
-$(function() {
-  var section = window.parent.location.hash.split('-')[1];
-  if (section) {
-    $('#develop-nav .' + section.toLowerCase()).addClass('selected');
+if (typeof Locker === 'undefined') Locker = {};
+
+Locker.Develop = (function() {
+  function init() {
+    selectCurrent();
+    handleLinks();
+    loadExternalDocs();
   }
 
-  $('#develop-nav a, a.iframeLink').click(function(e) {
-    e.preventDefault();
-    var link = $(this);
-    var href = link.attr('href');
-    if (href.indexOf('#') === 0) window.parent.location.hash = href;
-    else window.open(href, '_blank').focus();
-  });
+  function selectCurrent() {
+    var hash = window.parent.location.hash;
+    $('#develop-nav a[href=' + hash + ']').addClass('selected');
+  }
 
-  $('.lazyload').each(function(i, el) {
-    el = $(el);
-    $.get(el.data('src'), function(r) { el.html(r); });
-  });
+  function handleLinks() {
+    $('#develop-nav a, a.iframeLink').click(function(e) {
+      e.preventDefault();
+      var link = $(this);
+      var href = link.attr('href');
+      if (href.indexOf('#') === 0) window.parent.location.hash = href;
+      else window.open(href, '_blank').focus();
+    });
+  }
 
-});
+  function loadExternalDocs() {
+    $('.lazyload').each(function(i, el) {
+      el = $(el);
+      $.get(el.data('src'), function(r) { el.html(r); });
+    });
+  }
 
+  return {
+    init : init
+  };
+})();
 
-function isGitHubConnected(callback) {
-  $.getJSON('/map', function(map) {
-    return callback(map.github && map.github.auth && map.github.auth.profile && map.github.authed > 0);
-  });
-}
-
-function getGitHubProfile(callback) {
-  $.getJSON('/map/profiles', function(profiles) {
-    for(var i in profiles) if(i.indexOf('contact://github/') === 0) return callback(profiles[i]);
-    return callback();
-  });
-}
-
-function pollForGitHubProfile(callback) {
-  getGitHubProfile(function(profile) {
-    if(profile) return callback(profile);
-    setTimeout(function() {
-      pollForGitHubProfile(callback);
-    }, 1000);
-  });
-}
+$(Locker.Develop.init);
