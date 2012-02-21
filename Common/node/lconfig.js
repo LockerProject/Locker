084da392e8fafeba1194fa8d7ad0d9b2ab319415
Kristján Pétursson <kristjan@singly.com>
2012-02-20 22:17:25 -0800

    Revert "Add Gowalla connector"
    This reverts commit 589111de35348a5e665de3fa67a29691e3433431.
    
    Too optimistic; much has changed since this connector was written, and it needs
    to be brought in line. Look for a gowalla branch coming soon in repos near you.

--- Common/node/lconfig.js
+++ Common/node/lconfig.js
@@ -52,7 +52,6 @@ exports.load = function(filepath) {
         "flickr:Connectors/Flickr",
         "github:Connectors/GitHub",
         "gcontacts:Connectors/GoogleContacts",
-        "gowalla:Connectors/Gowalla",
         "instagram:Connectors/Instagram",
         "twitter:Connectors/Twitter",
         "foursquare:Connectors/foursquare",
