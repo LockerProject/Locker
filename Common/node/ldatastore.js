913fce5fe1264af9c037c9ef5d626e5ba1df6614
Simon Murtha-Smith <simon@murtha-smith.com>
2012-02-26 01:04:44 -0800

    make getMongo in ldatastore truly synchronous and throw errors, catch them in parents. add a bit of debug code to log deleteData and addData queue task if they have a bad callback and dont throw it (not ideal, but will get us through until we understand it better, v low risk overall) - for #873

--- Common/node/ldatastore.js
+++ Common/node/ldatastore.js
@@ -28,8 +28,7 @@ exports.init = function(owner, callback) {
 
 exports.addCollection = function(owner, name, dir, id) {
     mongoIDs[dir + "_" + name] = id;
-    if(!colls[owner][dir + "_" + name])
-    {
+    if(!colls[owner][dir + "_" + name]) {
         mongo[owner].addCollection(owner, dir + "_" + name);
         var ndx = {};
         ndx[id] = true;
@@ -47,7 +46,7 @@ exports.addCollection = function(owner, name, dir, id) {
 // timeStamp will be the timestamp stored w/ the record if it exists, otherwise, just use now.
 //
 exports.addObject = function(owner, type, object, options, callback) {
-    var timeStamp = now();
+    var timeStamp = Date.now();
     if (arguments.length == 3) callback = options;
     if (typeof options == 'object') {
         for (var i in options['strip']) {
@@ -68,7 +67,7 @@ exports.addObject = function(owner, type, object, options, callback) {
 
 // same deal, except no strip option, just timestamp is available currently
 exports.removeObject = function(owner, type, id, options, callback) {
-    var timeStamp = now();
+    var timeStamp = Date.now();
     if (arguments.length == 4) callback = options;
     if (typeof options == 'object') {
         if (options['timeStamp']) {
@@ -78,69 +77,90 @@ exports.removeObject = function(owner, type, id, options, callback) {
     var record = {deleted: timeStamp};
     record[mongoIDs[type]] = id;
     ijodFiles[type].addRecord(timeStamp, record, function(err) {
-        if (err)
-            callback(err);
+        if (err) return callback(err);
         removeCurrent(owner, type, id, callback);
     })
 }
 
 exports.queryCurrent = function(owner, type, query, options, callback) {
+  try {
     query = query || {};
     options = options || {};
     var m = getMongo(owner, type, callback);
     m.find(query, options).toArray(callback);
+  } catch(err) {
+    return callback(err);
+  }
 }
 
 exports.getAllCurrent = function(owner, type, callback, options) {
+  try {
     options = options || {};
     var m = getMongo(owner, type, callback);
     m.find({}, options).toArray(callback);
+  } catch(err) {
+    return callback(err);
+  }
 }
 
 exports.getEachCurrent = function(owner, type, callback, options) {
+  try {
     options = options || {};
     var m = getMongo(owner, type, callback);
     m.find({}, options).each(callback);
+  } catch(err) {
+    return callback(err);
+  }
 }
 
 exports.getCurrent = function(owner, type, id, callback) {
+  try {
     if (!(id && (typeof id === 'string' || typeof id === 'number')))  return callback(new Error('bad id:' + id), null);
-    var m = getMongo(owner, type, callback);
+    var m = getMongo(owner, type);
     var query = {_id: mongo[owner].db.bson_serializer.ObjectID(id)};
     m.findOne(query, callback);
+  } catch(err) {
+    return callback(err);
+  }
 }
 
 exports.getCurrentId = function(owner, type, id, callback) {
+  try {
     if (!(id && (typeof id === 'string' || typeof id === 'number')))  return callback(new Error('bad id:' + id), null);
-    var m = getMongo(owner, type, callback);
+    var m = getMongo(owner, type);
     var query = {"id":parseInt(id)};
     m.findOne(query, callback);
+  } catch(err) {
+    return callback(err);
+  }
 }
 
 
 function setCurrent(owner, type, object, callback) {
     if (type && object && callback && object[mongoIDs[type]]) {
+      try {
         var m = getMongo(owner, type, callback);
-        if(m) {
-            var query = {};
-            query[mongoIDs[type]] = object[mongoIDs[type]];
-            m.findAndModify(query, [['_id','asc']], object, {upsert:true, safe:true}, function(err, doc) {
-                if (deepCompare(doc, {})) {
-                    m.findOne(query, function(err, newDoc) {
-                        callback(err, 'new', newDoc);
-                    });
+        var query = {};
+        query[mongoIDs[type]] = object[mongoIDs[type]];
+        m.findAndModify(query, [['_id','asc']], object, {upsert:true, safe:true}, function(err, doc) {
+            if (deepCompare(doc, {})) {
+                m.findOne(query, function(err, newDoc) {
+                    callback(err, 'new', newDoc);
+                });
+            } else {
+                var id = doc._id;
+                delete doc._id;
+                if (deepCompare(doc, object)) {
+                    callback(err, 'same', doc);
                 } else {
-                    var id = doc._id;
-                    delete doc._id;
-                    if (deepCompare(doc, object)) {
-                        callback(err, 'same', doc);
-                    } else {
-                        doc._id = id;
-                        callback(err, 'update', doc);
-                    }
+                    doc._id = id;
+                    callback(err, 'update', doc);
                 }
-            });
-        }
+            }
+        });
+      } catch(err) {
+        return callback(err);
+      }
     } else {
         logger.error('failed to set current in ldatastore');
         logger.error(type)
@@ -150,28 +170,21 @@ function setCurrent(owner, type, object, callback) {
 }
 
 function removeCurrent(owner, type, id, callback) {
-    var m = getMongo(owner, type, callback);
-    if(m) {
-        var query = {};
-        query[mongoIDs[type]] = id;
-        m.remove(query, callback);
-    }
+  try {
+    var m = getMongo(owner, type);
+    var query = {};
+    query[mongoIDs[type]] = id;
+    m.remove(query, callback);
+  } catch(err) {
+    return callback(err);
+  }
 }
 
-function getMongo(owner, type, callback) {
+function getMongo(owner, type) {
     var m = colls[owner][type];
     if(!m) {
-        try {
-            mongo[owner].addCollection(owner, type);
-        } catch (E) {
-            return callback(E, []);
-        }
-        m = colls[owner][type];
+      mongo[owner].addCollection(owner, type);
+      m = colls[owner][type];
     }
     return m;
 }
-
-function now() {
-    return Date.now();
-}
-
