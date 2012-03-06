072248f252d3b3c2f43f0581808d315002671471
Matt Zimmerman <mdz@singly.com>
2012-03-06 15:37:26 -0800

    Parse mongo memory stats from its log messages

--- lockerd.js
+++ lockerd.js
@@ -93,7 +93,16 @@ path.exists(lconfig.me + '/' + lconfig.mongo.dataDir, function(exists) {
         logger.info('[mongo] ' + line);
         if (waitingForMongo && line.indexOf("[initandlisten] waiting for connections on port " + lconfig.mongo.port) >= 0) {
           waitingForMongo = false;
-          lmongo.connect(checkKeys);
+          return lmongo.connect(checkKeys);
+        }
+
+        var mongoMemoryReport = /[clientcursormon] mem \(MB\) res:(\d+) virt:(\d+) mapped:(\d+)/;
+        var match = line.match(mongoMemoryReport);
+        if (match) {
+            var mongoRSS = parseInt(match[1]),
+                mongoVirt = parseInt(match[2]),
+                mongoMapped = parseInt(match[3]);
+            console.log("Mongo res: "+mongoRSS+", virt: "+mongoVirt+", mapped: "+mongoMapped);
         }
     });
     var mongoStderr = carrier.carry(mongoProcess.stderr);
