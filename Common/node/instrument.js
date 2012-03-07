21b4161d840d7c45a3c376504a714ac90fac6bce
Paul Oppenheim <paul@pauloppenheim.com>
2012-03-06 19:48:11 -0800

    Fix crash on 'example.com' DNS entry in config

--- Common/node/instrument.js
+++ Common/node/instrument.js
@@ -7,6 +7,7 @@
  *
  */
 var dgram  = require('dgram')
+  , util = require('util')
   , logger = require('./logger.js');
 
 function StatsdDispatcher(config) {
@@ -21,11 +22,17 @@ StatsdDispatcher.prototype.send = function (msg) {
     return;
   }
   if (this.prefix) msg = this.prefix + '.' + msg;
-
+  
+  // create closure; if there's an error, can report the problematic host/port
+  f_ctx = this;
+  var f = function () {with(f_ctx){ return [f_ctx.host, f_ctx.port, f_ctx.prefix];}}
+  
   var socket = dgram.createSocket('udp4');
   var buf = new Buffer(msg);
   socket.send(buf, 0, buf.length, this.port, this.host, function (err, bytes) {
-    if (err) console.error('statsd error: ' + err);
+    if (err) {
+      console.error('statsd error: %s --- %s', err, util.inspect(f()));
+    }
 
     socket.close();
   });
