fbd40692b68ad6a250dcbd160460bdc097e15073
Thomas Muldowney <temas@sparecog.com>
2012-03-13 10:11:10 -0500

    Make sure we have some indexes on the photos collection

--- Collections/Photos/dataStore.js
+++ Collections/Photos/dataStore.js
@@ -24,6 +24,9 @@ exports.init = function(mongo, _locker) {
     collection = mongo.collections.photo;
     collectionDataStore.init(mongo, 'photo', locker);
 
+    collection.ensureIndex({url:1});
+    collection.ensureIndex({name:1});
+
     db = mongo.dbClient;
     logger = require("logger");
 }
