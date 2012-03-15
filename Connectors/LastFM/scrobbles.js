c20d29f784505d737405308e1543bfb80d4925ed
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-03-14 18:13:41 -0700

    Don't save the currently playing track.
    When the scrobbles synclet runs, it needs to generate a key for each
    play. It uses the timestamp, as that's guaranteed (by the rules of
    scrobbling) to be distinct for each track. However, when the synclet
    fires while the locker owner is listening to music through a service
    that scrobbles, it pushes the current track on to the head of the
    scrobble list with the attribute nowPlaying and no timestamp.
    
    The synclet manager chokes if it gets entities without timestamps, and
    it doesn't make any sense to save an ephemeral record anyway, so don't
    save tracks that are currently playing or otherwise missing a timestamp.

--- Connectors/LastFM/scrobbles.js
+++ Connectors/LastFM/scrobbles.js
@@ -13,7 +13,6 @@ exports.sync = function (processInfo, cb) {
   var lastfm = require(path.join(processInfo.absoluteSrcdir, 'lib.js'));
   var scrobbles = [];
 
-
   if (!processInfo.config) processInfo.config = {};
   if (!processInfo.config.playcount) processInfo.config.playcount = 0;
 
@@ -22,9 +21,14 @@ exports.sync = function (processInfo, cb) {
   if (processInfo.config && processInfo.config.lastplayed) {
     params.from = processInfo.config.lastplayed;
   }
+
   lastfm.getScrobbles(processInfo, params, function (play) {
-    if (play && play.date && play.date.uts) play.id = play.date.uts;
-    scrobbles.push(play);
+    if (play && play.date && play.date.uts) {
+      play.id = play.date.uts;
+      // we only save this track if it has an ID -- currently-playing tracks will
+      // never have a timestamp, and therefore no ID.
+      scrobbles.push(play);
+    }
   }, function (err, config) {
     if (err) {
       console.error("Error! %s", err);
