acd4054f43aa3ac4a834a0efa23a95fa93dd483e
Thomas Muldowney <temas@sparecog.com>
2012-03-15 21:14:52 -0500

    Pick a tar program, defaulting to gnutar, then tar

--- scripts/git-archive-all.sh
+++ scripts/git-archive-all.sh
@@ -100,6 +100,15 @@ TOARCHIVE=`mktemp "$TMPDIR/$PROGRAM.toarchive.XXXXXX"`
 OUT_FILE=$OLD_PWD # assume "this directory" without a name change by default
 SEPARATE=0
 
+TAR=`which gnutar`
+if [ -z $TAR ]; then
+  TAR=`which tar`
+  if [ -z $TAR ]; then
+    echo "Unable to find a valid tar program."
+    exit 1;
+  fi
+fi
+
 FORMAT=tar
 PREFIX=
 TREEISH=HEAD
@@ -191,7 +200,7 @@ done < $TOARCHIVE
 if [ $SEPARATE -eq 0 ]; then
     if [ $FORMAT == 'tar' ]; then
         sed -e '1d' $TMPFILE | while read file; do
-            gnutar --concatenate -f "$superfile" "$file" && rm -f "$file"
+            $TAR --concatenate -f "$superfile" "$file" && rm -f "$file"
         done
     elif [ $FORMAT == 'zip' ]; then
         sed -e '1d' $TMPFILE | while read file; do
