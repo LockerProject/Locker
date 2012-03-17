8b8cdefc20dd9fed8f99b968580438bbfbe315f5
Beau Gunderson <beau@beaugunderson.com>
2012-03-16 22:49:48 -0700

    Adding the Netflix connector (with 1 synclet: viewing history)

---
+++ Connectors/Netflix/test.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var auth = JSON.parse(fs.readFileSync("../../Me/netflix/me.json")).auth;
+
+console.error('auth', auth);
+
+var sync = require(process.argv[2]);
+
+sync.sync({ auth: auth }, function(e, js) {
+  console.error('error', e);
+
+  console.error("got js", JSON.stringify(js));
+});
