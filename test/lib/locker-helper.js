5c391c937ece84be44709a7e7530841851f1c368
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-03-20 11:43:04 -0700

    More complete locker control.
    Shutdown still isn't working properly for some reason, and using
    environment variables rather than cleaner communication of configuration
    data at startup doesn't feel clean. Until we come up with a better
    mechanism than lconfig, though, this is what we have.

--- test/lib/locker-helper.js
+++ test/lib/locker-helper.js
@@ -8,14 +8,17 @@ var lconfig
 
 exports.configurate = function () {
   if (!lconfig) {
+    // override from the system temporary directory because of the locker's insane insistence on relative paths.
+    temp.dir = '.';
+
     process.env.NODE_PATH = path.join(__dirname, '..', '..', 'Common', 'node');
 
-    lconfig = require(path.join(__dirname, '..', '..', 'Common', 'node', 'lconfig.js'));
-    lconfig.load(path.join(__dirname, '..', 'resources', 'config.json'));
+    process.env.LOCKER_ROOT = path.join(__dirname, '..', '..');
+    process.env.LOCKER_CONFIG = path.join(__dirname, '..', 'resources');
+    process.env.LOCKER_ME = temp.path({prefix : 'Me.',
+                                       suffix : '.test'});
 
-    temp.dir = '.'; // override from the system temporary directory because of the locker's insane insistence on relative paths.
-    lconfig.me = temp.path({prefix : 'Me.', suffix : '.test'});
-    lconfig.lockerDir = path.join(__dirname, '..', '..');
+    lconfig = require(path.join(__dirname, '..', '..', 'Common', 'node', 'lconfig.js'));
   }
 
   return lconfig;
@@ -46,6 +49,17 @@ exports.lockerificate = function (done) {
   return waitOnLocker(done);
 };
 
-exports.bootstrapLocker = function (done) {
+exports.delockerificate = function (done) {
+  if (locker) {
+    locker.shutdown(0);
+  }
+  return done();
+};
+
+exports.bootstrap = function (done) {
   exports.lockerificate(done);
+};
+
+exports.shutdown = function (done) {
+  exports.delockerificate(done);
 };
